f8ec235caa136896143e67824b437d4ca69765c1
takumus <me@takumus.io>
2021-09-24 09:52:03 +0900

    プッシュのやつもやるようにした。

--- scripts/release.js
+++ scripts/release.js
@@ -2,4 +2,5 @@ const fs = require("fs");
 const process = require("child_process");
 const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
 console.log("VERSION:", packageJSON.version);
-process.exec(`git tag ${packageJSON.version}`);
+console.log(process.execSync(`git tag ${packageJSON.version}`).toString());
+console.log(process.execSync(`git push origin --tags`).toString());
