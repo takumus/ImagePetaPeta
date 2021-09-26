26476030534b397c8cc092403ea28e1eaea7d449
takumus <me@takumus.io>
2021-09-27 05:08:42 +0900

    おｋ

--- src/globals/globalSettings.ts
+++ src/globals/globalSettings.ts
@@ -1,14 +1,14 @@
 import { defaultSettings, Settings } from "@/datas/settings";
 import { reactive, App } from "vue";
-const reactivedSettings: Settings = reactive(defaultSettings);
+const settings: Settings = reactive(defaultSettings);
 const Plugin = {
   install(app: App) {
-    app.config.globalProperties.$settings = reactivedSettings;
+    app.config.globalProperties.$settings = settings;
   }
 }
 export default Plugin;
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
-    $settings: typeof reactivedSettings;
+    $settings: typeof settings;
   }
 }
