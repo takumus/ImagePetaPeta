ebb79b61e57ad0e79edc9e55cd1487f513143f25
takumus <me@takumus.io>
2021-10-01 02:03:18 +0900

    ライセンス追加

--- scripts/release.js
+++ scripts/release.js
@@ -4,3 +4,5 @@ const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
 console.log("VERSION:", packageJSON.version);
 console.log(process.execSync(`git tag ${packageJSON.version}`).toString());
 console.log(process.execSync(`git push origin --tags`).toString());
+const readme = fs.readFileSync("README.md").toString().replace(/<https:\/\/github.com\/takumus\/ImagePetaPeta\/releases\/tag\/.*>/g, `https://github.com/takumus/ImagePetaPeta/releases/tag/${packageJSON.version}`);
+fs.writeFileSync("README.md", readme);
