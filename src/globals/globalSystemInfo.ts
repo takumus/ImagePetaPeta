b29f62157ea392b5c6c1a73e0f2e0721ff827173
takumus <me@takumus.io>
2021-11-02 22:40:42 +0900

    システム情報もグローバルに

---
+++ src/globals/globalSystemInfo.ts
@@ -0,0 +1,19 @@
+import { API } from "@/api";
+import { reactive, App } from "vue";
+const systemInfo: {
+  platform: NodeJS.Platform
+} = reactive({
+  platform: "win32"
+});
+const Plugin = {
+  async install(app: App) {
+    app.config.globalProperties.$systemInfo = systemInfo;
+    systemInfo.platform = await API.send("getPlatform");
+  }
+}
+export default Plugin;
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    $systemInfo: typeof systemInfo
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,20 @@ import { createApp } from "vue"
 import { createI18n } from "vue-i18n";
 import languages from "@/languages";
 import App from "@/components/VIndex.vue"
+import { App as _App } from "vue";
 import GlboalKeyboard from "@/globals/globalKeyboard";
 import GlobalSettings from "@/globals/globalSettings";
-
-const app = createApp(App);
-const i18n = createI18n({
-  locale: "ja",
-  messages: languages,
-});
-
-app.use(i18n);
-app.use(GlboalKeyboard);
-app.use(GlobalSettings);
-app.config.globalProperties.$globalComponents = {};
-
-app.mount("#app");
+import GlobalSystemInfo from "@/globals/globalSystemInfo";
+(async () => {
+  const app = createApp(App);
+  const i18n = createI18n({
+    locale: "ja",
+    messages: languages,
+  });
+  app.use(i18n);
+  app.use(GlboalKeyboard);
+  await GlobalSettings.install(app);
+  await GlobalSystemInfo.install(app);
+  app.config.globalProperties.$globalComponents = {};
+  app.mount("#app");
+})();
