0d16067fb75e8792bbf8a2e381b9e0b41a4a81e5
takumus <me@takumus.io>
2022-04-13 01:43:25 +0900

    undefinedでもいいや。

--- src/rendererProcess/utils/imageURL.ts
+++ src/rendererProcess/utils/imageURL.ts
@@ -1,5 +1,5 @@
 import { PetaImage } from "@/commons/datas/petaImage";
 import { ImageType } from "@/commons/datas/imageType";
-export function getImageURL(petaImage: PetaImage, type: ImageType) {
-  return `image-${type}://${type == ImageType.ORIGINAL ? petaImage.file.original : petaImage.file.thumbnail}`;
+export function getImageURL(petaImage: PetaImage | undefined, type: ImageType) {
+  return `image-${type}://${type == ImageType.ORIGINAL ? petaImage?.file.original : petaImage?.file.thumbnail}`;
 }
