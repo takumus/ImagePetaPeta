fa6a0d94ba99db5ce315abda664c9fd8bfac7f0a
takumus <me@takumus.io>
2022-04-27 23:39:55 +0900

    リリース周り修正。

---
+++ scripts/addBinaryHash.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
+const isMac = process.platform == "darwin";
+const binaryFile = isMac ? `./dist_electron/ImagePetaPeta-beta Setup ${packageJSON.version}.exe` : `./dist_electron/mac-arm64/ImagePetaPeta-beta.app`;
+const hashAdded = fs.readFileSync("./package.json")
+.toString()
+.replace(
+  /"binary-sha256-win": ".*"/, 
+  `"binary-sha256-win": "${crypto.createHash("sha256").update(fs.readFileSync(binaryFile)).digest("hex")}"`
+)
+fs.writeFileSync("./package.json", hashAdded);
---
+++ scripts/commitAndAddTag.js
@@ -0,0 +1,8 @@
+const process = require("child_process");
+const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
+
+console.log("VERSION:", packageJSON.version);
+console.log(process.execSync(`git add .`).toString());
+console.log(process.execSync(`git commit -m "Update version ${packageJSON.version}"`).toString());
+console.log(process.execSync(`git tag ${packageJSON.version}`).toString());
+// console.log(process.execSync(`git push origin --tags`).toString());
