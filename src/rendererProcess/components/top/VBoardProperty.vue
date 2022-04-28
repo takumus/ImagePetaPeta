4ac4b1b223637f617fce406523b8e585d6438f1a
takumus <me@takumus.io>
2022-04-29 06:39:21 +0900

    Update VBoardProperty.vue

--- src/rendererProcess/components/top/VBoardProperty.vue
+++ src/rendererProcess/components/top/VBoardProperty.vue
@@ -109,98 +109,99 @@ v-board-property-root {
   width: 100%;
   position: absolute;
   z-index: 2;
-    background-color: var(--tab-selected-color);
-    height: 30px;
-    box-shadow: -1px 2px 2px 0px rgba(0, 0, 0, 0.4);
-    border-bottom: solid 1px var(--bg-color);
-    >v-shared {
-      position: absolute;
-      top: 0px;
-      height: 100%;
-      padding: 4px;
-      display: flex;
-      flex-shrink: 1;
-      text-align: left;
-      justify-content: flex-end;
-      &.left {
-        left: 0px;
-        button {
-          margin-right: 4px;
-        }
-      }
-      &.right {
-        right: 0px;
-        button {
-          margin-left: 4px;
-        }
-      }
-      &.left, &.right {
-        >button {
-          min-width: 24px;
-          padding: 0px;
-          height: 100%;
-          margin: 0px;
-          border: none;
-          v-icon {
-            display: block;
-            width: 100%;
-            height: 100%;
-            background-size: 14px;
-            background-repeat: no-repeat;
-            background-position: center center;
-            filter: var(--icon-filter);
-            &.browser {
-              background-image: url("~@/@assets/browser.png");
-            }
-            &.import-file {
-              background-image: url("~@/@assets/importFile.png");
-            }
-            &.import-folder {
-              background-image: url("~@/@assets/importFolder.png");
-            }
-            &.settings {
-              background-image: url("~@/@assets/settings.png");
-            }
-            &.info {
-              background-image: url("~@/@assets/info.png");
-            }
-          }
-        }
+  background-color: var(--tab-selected-color);
+  height: 30px;
+  box-shadow: -1px 2px 2px 0px rgba(0, 0, 0, 0.4);
+  border-bottom: solid 1px var(--bg-color);
+  display: block;
+  >v-shared {
+    position: absolute;
+    top: 0px;
+    height: 100%;
+    padding: 4px;
+    display: flex;
+    flex-shrink: 1;
+    text-align: left;
+    justify-content: flex-end;
+    &.left {
+      left: 0px;
+      button {
+        margin-right: 4px;
       }
     }
-    >v-property {
-      position: absolute;
-      top: 0px;
-      left: 0px;
-      height: 100%;
-      width: 100%;
-      padding: 4px;
-      padding-left: 6px;
-      text-align: center;
-      >input {
-        display: inline-block;
-        width: 0px;
-        height: 0px;
-        overflow: hidden;
-        position: relative;
-        margin: 0px;
-        padding: 0px;
-        border: none;
+    &.right {
+      right: 0px;
+      button {
+        margin-left: 4px;
       }
+    }
+    &.left, &.right {
       >button {
         min-width: 24px;
         padding: 0px;
         height: 100%;
         margin: 0px;
-        min-width: 50px;
-        margin-right: 4px;
-        &.color {
-          border-radius: 100px;
-          width: auto;
-          min-width: 0px;
-          aspect-ratio: 1;
+        border: none;
+        v-icon {
+          display: block;
+          width: 100%;
+          height: 100%;
+          background-size: 14px;
+          background-repeat: no-repeat;
+          background-position: center center;
+          filter: var(--icon-filter);
+          &.browser {
+            background-image: url("~@/@assets/browser.png");
+          }
+          &.import-file {
+            background-image: url("~@/@assets/importFile.png");
+          }
+          &.import-folder {
+            background-image: url("~@/@assets/importFolder.png");
+          }
+          &.settings {
+            background-image: url("~@/@assets/settings.png");
+          }
+          &.info {
+            background-image: url("~@/@assets/info.png");
+          }
         }
       }
     }
+  }
+  >v-property {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 100%;
+    width: 100%;
+    padding: 4px;
+    padding-left: 6px;
+    text-align: center;
+    >input {
+      display: inline-block;
+      width: 0px;
+      height: 0px;
+      overflow: hidden;
+      position: relative;
+      margin: 0px;
+      padding: 0px;
+      border: none;
+    }
+    >button {
+      min-width: 24px;
+      padding: 0px;
+      height: 100%;
+      margin: 0px;
+      min-width: 50px;
+      margin-right: 4px;
+      &.color {
+        border-radius: 100px;
+        width: auto;
+        min-width: 0px;
+        aspect-ratio: 1;
+      }
+    }
+  }
 }
 </style>
