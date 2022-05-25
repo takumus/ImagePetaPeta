d9c92727b2b1d0e5abdd0c2926f7f5f31ab7c0e0
takumus <me@takumus.io>
2022-05-25 11:15:00 +0200

    Update generateLicenses.js

--- scripts/generateLicenses.js
+++ scripts/generateLicenses.js
@@ -37,7 +37,7 @@ const newDatas = Object.keys(datas).map((name) => {
   }
 });
 fs.rmSync("./licenses.json");
-fs.writeFileSync("./src/@assets/licenses.ts", `export const LICENSES = ${JSON.stringify(newDatas, null, 2)}`.replace(/\r\n|\n|\r/g, "\n"), {
+fs.writeFileSync("./src/@assets/licenses.ts", `export const LICENSES = ${JSON.stringify(newDatas, null, 2)}`, {
   encoding: "utf-8",
 });
 console.log(Object.keys(licensesCounts).map((key) => `- ${key}: ${licensesCounts[key]}`).join("\n"));
