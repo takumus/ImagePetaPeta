64d9b7711664ac557a9a5ffc0e435a519761c4f1
takumus <me@takumus.io>
2022-06-22 23:11:53 +0900

    VDetailsは。VBoardをほぼそのまま、使えるかも。

--- src/rendererProcess/components/VDetailsIndex.vue
+++ src/rendererProcess/components/VDetailsIndex.vue
@@ -12,13 +12,16 @@
         </VUtilsBar> -->
       </t-top>
       <t-browser>
-        AAA
+        <VBoard
+          :zIndex="1"
+          :board="board"
+          ref="vPetaBoard"
+        />
       </t-browser>
     </t-content>
     <t-modals
       v-show="this.$components.modal.modalIds.length > 0"
     >
-      <VImageImporter />
       <VTasks />
     </t-modals>
     <VDialog
@@ -38,53 +41,84 @@
 import { Options, Vue } from "vue-class-component";
 import { Ref, Watch } from "vue-property-decorator";
 // Components
-import VImageImporter from "@/rendererProcess/components/importer/VImageImporter.vue";
 import VTasks from "@/rendererProcess/components/task/VTasks.vue";
 import VTitleBar from "@/rendererProcess/components/top/VTitleBar.vue";
 import VUtilsBar from "@/rendererProcess/components/top/VUtilsBar.vue";
 import VContextMenu from "@/rendererProcess/components/utils/VContextMenu.vue";
 import VComplement from "@/rendererProcess/components/utils/VComplement.vue";
 import VDialog from "@/rendererProcess/components/utils/VDialog.vue";
-import VSearch from "@/rendererProcess/components/browser/search/VSearch.vue";
+import VBoard from "@/rendererProcess/components/board/VBoard.vue";
 // Others
 import { API } from "@/rendererProcess/api";
-import { dbPetaImagesToPetaImages, dbPetaImageToPetaImage, PetaImages } from "@/commons/datas/petaImage";
+import { dbPetaImagesToPetaImages, dbPetaImageToPetaImage, PetaImage, PetaImages } from "@/commons/datas/petaImage";
 import { PetaTagInfo } from "@/commons/datas/petaTagInfo";
 import { logChunk } from "@/rendererProcess/utils/rendererLogger";
 import { PetaTag } from "@/commons/datas/petaTag";
 import { UpdateMode } from "@/commons/api/interfaces/updateMode";
+import { getImageURL } from "../utils/imageURL";
+import { ImageType } from "@/commons/datas/imageType";
+import { PetaBoard } from "@/commons/datas/petaBoard";
+import { Vec2 } from "@/commons/utils/vec2";
+import { BOARD_DARK_BACKGROUND_FILL_COLOR, BOARD_DARK_BACKGROUND_LINE_COLOR } from "@/commons/defines";
+import { PetaPanel } from "@/commons/datas/petaPanel";
 @Options({
   components: {
-    VImageImporter,
     VTasks,
     VTitleBar,
     VContextMenu,
     VComplement,
     VDialog,
     VUtilsBar,
-    VSearch
+    VBoard
   },
 })
 export default class DetailsIndex extends Vue {
+  @Ref("vPetaBoard")
+  vPetaBoard!: VBoard;
   petaImages: PetaImages = {};
   petaTagInfos: PetaTagInfo[] = [];
   selectedPetaTags: PetaTag[] = [];
   title = "";
+  petaImageId? = "";
   async mounted() {
-    API.on("updatePetaImages", (e, petaImages, mode) => {
+    API.on("updatePetaImages", async (e, petaImages, mode) => {
       if (mode === UpdateMode.UPSERT) {
         petaImages.forEach((petaImage) => {
           this.petaImages[petaImage.id] = dbPetaImageToPetaImage(petaImage);
+          this.board?.petaPanels.forEach((petaPanel) => {
+            if (petaPanel.petaImageId === petaImage.id) {
+              petaPanel._petaImage = this.petaImages[petaImage.id];
+              this.vPetaBoard.load();
+            }
+          });
         });
+      } else if (mode === UpdateMode.UPDATE) {
+        petaImages.forEach((newPetaImage) => {
+          const petaImage = this.petaImages[newPetaImage.id];
+          if (petaImage) {
+            Object.assign(petaImage, dbPetaImageToPetaImage(newPetaImage));
+          }
+        });
+        this.vPetaBoard.orderPIXIRender();
       } else if (mode === UpdateMode.REMOVE) {
         petaImages.forEach((petaImage) => {
           delete this.petaImages[petaImage.id];
+          this.board?.petaPanels.forEach((petaPanel) => {
+            if (petaPanel.petaImageId === petaImage.id) {
+              petaPanel._petaImage = undefined;
+              this.vPetaBoard.load();
+            }
+          });
         });
       }
     });
     API.on("updatePetaTags", (e) => {
       this.getPetaTagInfos();
     });
+    API.on("detailsPetaImage", (event, petaImage) => {
+      this.petaImageId = petaImage.id;
+    });
+    this.petaImageId = (await API.send("getDetailsPetaImage"))?.id;
     this.title = `${this.$appInfo.name} ${this.$appInfo.version}`;
     document.title = this.title;
     await this.getPetaImages();
@@ -92,6 +126,54 @@ export default class DetailsIndex extends Vue {
     this.$nextTick(() => {
       API.send("showMainWindow");
     });
+  }
+  get petaImage() {
+    if (this.petaImageId === undefined) {
+      return undefined;
+    }
+    return this.petaImages[this.petaImageId];
+  }
+  get board(): PetaBoard | undefined {
+    if (this.petaImage === undefined) {
+      return undefined;
+    }
+    const width = 256;
+    const panel: PetaPanel = {
+      petaImageId: this.petaImage.id,
+      position: new Vec2(),
+      rotation: 0,
+      width: width,
+      height: this.petaImage.height * width,
+      crop: {
+        position: new Vec2(0, 0),
+        width: 1,
+        height: 1
+      },
+      id: "petaImage",
+      index: 0,
+      gif: {
+        stopped: false,
+        frame: 0
+      },
+      visible: true,
+      locked: true,
+      _petaImage: this.petaImage
+    }
+    const board: PetaBoard = {
+      petaPanels: [panel],
+      id: "details",
+      name: "details",
+      transform: {
+        scale: 1,
+        position: new Vec2(0, 0)
+      },
+      background: {
+        fillColor: BOARD_DARK_BACKGROUND_FILL_COLOR,
+        lineColor: BOARD_DARK_BACKGROUND_LINE_COLOR,
+      },
+      index: 0
+    }
+    return board;
   }
   async getPetaImages() {
     this.petaImages = dbPetaImagesToPetaImages(await API.send("getPetaImages"), false);
@@ -105,6 +187,13 @@ export default class DetailsIndex extends Vue {
       return this.$systemDarkMode.value;
     }
     return this.$settings.darkMode;
+  }
+  get petaImageURL() {
+    return getImageURL(this.petaImage, ImageType.ORIGINAL);
+  }
+  @Watch("petaImage")
+  changePetaImage() {
+    console.log(this.petaImage);
   }
   @Watch("$focusedWindows.focused")
   changeWindowIsFocused() {
