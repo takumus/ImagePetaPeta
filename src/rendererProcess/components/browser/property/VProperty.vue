696776ae08528af79b0dc7825bf6c72b8d177e96
takumus <me@takumus.io>
2022-08-16 22:24:59 +0900

    試しにComposition APIに載せ替え。

--- src/rendererProcess/components/browser/property/VProperty.vue
+++ src/rendererProcess/components/browser/property/VProperty.vue
@@ -94,7 +94,7 @@
           type="checkbox"
           tabindex="-1"
           :checked="nsfw"
-          @change="changeNSFW(Boolean($event.target.checked))"
+          @change="changeNSFW(Boolean(($event.target as HTMLInputElement).checked))"
         >
           NSFW
         </label>
@@ -102,10 +102,10 @@
   </t-property-root>
 </template>
 
-<script lang="ts">
-// Vue
-import { Options, Vue } from "vue-class-component";
-import { Prop, Ref, Watch } from "vue-property-decorator";
+<script setup lang="ts">
+// // Vue
+// import { Options, Vue } from "vue-class-component";
+// import { Prop, Ref, Watch } from "vue-property-decorator";
 // Components
 import VEditableLabel from "@/rendererProcess/components/utils/VEditableLabel.vue";
 import VPropertyThumbnail from "@/rendererProcess/components/browser/property/VPropertyThumbnail.vue";
@@ -120,185 +120,178 @@ import { updatePetaImages } from "@/rendererProcess/utils/updatePetaImages";
 import { createPetaTag, PetaTag } from "@/commons/datas/petaTag";
 import { PetaTagInfo } from "@/commons/datas/petaTagInfo";
 import dateFormat from "dateformat";
-@Options({
-  components: {
-    VEditableLabel,
-    VPropertyThumbnail
-  },
-  emits: [
-    "selectTag"
-  ]
-})
-export default class VProperty extends Vue {
-  @Prop()
-  petaImages!: PetaImage[];
-  @Prop()
-  petaTagInfos!: PetaTagInfo[];
-  fetchingTags = true;
-  note = "";
-  @Ref()
-  noteTextArea!: HTMLTextAreaElement;
-  mounted() {
-    //
-  }
-  unmounted() {
-    //
-  }
-  async addTag(name: string) {
-    // タグを探す。なかったら作る。
-    let petaTag = this.petaTagInfos.find((pti) => pti.petaTag.name === name)?.petaTag;
-    // リクエスト2回飛ばさない
-    if (!petaTag) {
-      petaTag = createPetaTag(name);
-      await API.send(
-        "updatePetaTags",
-        [petaTag],
-        UpdateMode.UPSERT
-      );
-    }
+import { computed, nextTick, ref, watch, defineEmits, defineProps, getCurrentInstance } from "vue";
+
+const emit = defineEmits<{
+  (e: "selectTag", tag: PetaTag): void
+}>();
+const props = defineProps<{
+  petaImages: PetaImage[],
+  petaTagInfos: PetaTagInfo[],
+}>();
+let fetchingTags = ref(false);
+let note = ref("");
+let sharedPetaTags = ref<PetaTag[]>([]);
+const noteTextArea = ref<HTMLTextAreaElement>();
+const _this = getCurrentInstance()!.proxy!;
+async function addTag(name: string) {
+  // タグを探す。なかったら作る。
+  let petaTag = props.petaTagInfos.find((pti) => pti.petaTag.name === name)?.petaTag;
+  // リクエスト2回飛ばさない
+  if (!petaTag) {
+    petaTag = createPetaTag(name);
     await API.send(
-      "updatePetaImagesPetaTags",
-      this.petaImages.map((petaImage) => petaImage.id),
-      [petaTag.id],
+      "updatePetaTags",
+      [petaTag],
       UpdateMode.UPSERT
     );
   }
-  async removeTag(petaTag: PetaTag) {
-    await API.send(
-      "updatePetaImagesPetaTags",
-      this.petaImages.map((petaImage) => petaImage.id),
-      [petaTag.id],
-      UpdateMode.REMOVE
-    );
-  }
-  resizeNote() {
-    this.noteTextArea.style.height = this.noteTextArea.scrollHeight + 'px';//この行だけでOK？
-    this.noteTextArea.style.height = "auto";
-    this.$nextTick(()=>{
-      this.noteTextArea.style.height = this.noteTextArea.scrollHeight + 'px';
-    })
-  }
-  changeName(name: string) {
-    if (this.singlePetaImageInfo === undefined) {
-      return;
-    }
-    this.singlePetaImageInfo.petaImage.name = name;
-    API.send("updatePetaImages", [this.singlePetaImageInfo.petaImage], UpdateMode.UPDATE);
-  }
-  blurNoteTextarea(e: KeyboardEvent) {
-    const textarea = e.target as HTMLTextAreaElement;
-    this.changeNote(textarea.value);
-  }
-  changeNote(note: string) {
-    if (this.singlePetaImageInfo === undefined) {
-      return;
-    }
-    if (this.singlePetaImageInfo.petaImage.note === note) {
-      return;
-    }
-    this.singlePetaImageInfo.petaImage.note = note;
-    API.send("updatePetaImages", [this.singlePetaImageInfo.petaImage], UpdateMode.UPDATE);
-  }
-  changeNSFW(value: boolean) {
-    this.petaImages.forEach((pi, i) => {
-      pi.nsfw = value;
-    });
-    updatePetaImages(this.petaImages, UpdateMode.UPDATE);
-  }
-  clearSelection() {
-    this.petaImages.forEach((pi) => {
-      pi._selected = false;
-    })
-  }
-  complementTag(editableLabel: VEditableLabel) {
-    this.$components.complement.open(editableLabel, this.petaTagInfos.filter((pti) => {
-      return pti.petaTag.id !== UNTAGGED_ID;
-    }).map((pti) => {
-      return pti.petaTag.name;
-    }));
-  }
-  tagMenu(event: PointerEvent, tag: PetaTag) {
-    this.$components.contextMenu.open([
-      {
-        label: this.$t("browser.property.tagMenu.remove", [tag.name]),
-        click: () => {
-          this.removeTag(tag);
-        }
-      }
-    ], vec2FromPointerEvent(event));
-  }
-  selectTag(tag: PetaTag) {
-    this.$emit("selectTag", tag);
-  }
-  sharedPetaTags: PetaTag[] = [];
-  async fetchPetaTags() {
-    this.fetchingTags = true;
-    if (this.noImage) {
-      this.sharedPetaTags = [];
-      this.fetchingTags = false;
-      return;
-    }
-    const result = await API.send("getPetaTagIdsByPetaImageIds", this.petaImages.map((petaImage) => petaImage.id));
-    this.sharedPetaTags = this.petaTagInfos.filter((pti) => result.find((id) => id === pti.petaTag.id)).map((pi) => pi.petaTag);
-    this.fetchingTags = false;
-  }
-  get singlePetaImageInfo() {
-    if (this.petaImages.length === 1) {
-      const petaImage = this.petaImages[0]!;
-      const all = petaImage.palette.reduce((p, c) => {
-        return p + c.population;
-      }, 0);
-      return {
-        name: petaImage.name,
-        petaImage: petaImage,
-        fileDate: dateFormat(petaImage.fileDate, "yyyy/mm/dd hh:MM:ss"),
-        addDate: dateFormat(petaImage.addDate, "yyyy/mm/dd hh:MM:ss"),
-        palette: petaImage.palette.map((color, i) => {
-          return {
-            color: `rgb(${color.r}, ${color.g}, ${color.b})`,
-            population: color.population / all,
-            id: i
-          };
-        })
-      }
-    }
-    return undefined;
-  }
-  get noImage() {
-    return this.petaImages.length === 0;
-  }
-  get nsfw() {
-    let nsfw = false;
-    let same = true;
-    this.petaImages.forEach((pi, i) => {
-      if (i === 0) {
-        nsfw = pi.nsfw;
-        return;
-      }
-      if (nsfw != pi.nsfw) {
-        same = false;
-      }
-    })
-    if (same) {
-      return nsfw;
-    }
-    return "not-same";
-  }
-  @Watch("petaImages")
-  changePetaImages() {
-    this.fetchPetaTags();
-    if (this.singlePetaImageInfo) {
-      this.note = this.singlePetaImageInfo.petaImage.note;
-      this.$nextTick(this.resizeNote);
-    } else {
-      this.note = "";
-    }
-  }
-  @Watch("petaTagInfos")
-  changeFetchTags() {
-    this.fetchPetaTags();
-  }
+  await API.send(
+    "updatePetaImagesPetaTags",
+    props.petaImages.map((petaImage) => petaImage.id),
+    [petaTag.id],
+    UpdateMode.UPSERT
+  );
 }
+async function removeTag(petaTag: PetaTag) {
+  await API.send(
+    "updatePetaImagesPetaTags",
+    props.petaImages.map((petaImage) => petaImage.id),
+    [petaTag.id],
+    UpdateMode.REMOVE
+  );
+}
+function resizeNote() {
+  if (noteTextArea.value === undefined) {
+    return;
+  }
+  const _noteTextArea = noteTextArea.value;
+  _noteTextArea.style.height = _noteTextArea.scrollHeight + 'px';//この行だけでOK？
+  _noteTextArea.style.height = "auto";
+  nextTick(()=>{
+    _noteTextArea.style.height = _noteTextArea.scrollHeight + 'px';
+  })
+}
+function changeName(name: string) {
+  if (singlePetaImageInfo.value === undefined) {
+    return;
+  }
+  singlePetaImageInfo.value.petaImage.name = name;
+  API.send("updatePetaImages", [singlePetaImageInfo.value.petaImage], UpdateMode.UPDATE);
+}
+function blurNoteTextarea(e: FocusEvent) {
+  const textarea = e.target as HTMLTextAreaElement;
+  changeNote(textarea.value);
+}
+function changeNote(note: string) {
+  if (singlePetaImageInfo.value === undefined) {
+    return;
+  }
+  if (singlePetaImageInfo.value.petaImage.note === note) {
+    return;
+  }
+  singlePetaImageInfo.value.petaImage.note = note;
+  API.send("updatePetaImages", [singlePetaImageInfo.value.petaImage], UpdateMode.UPDATE);
+}
+function changeNSFW(value: boolean) {
+  props.petaImages.forEach((pi, i) => {
+    pi.nsfw = value;
+  });
+  updatePetaImages(props.petaImages, UpdateMode.UPDATE);
+}
+function clearSelection() {
+  props.petaImages.forEach((pi) => {
+    pi._selected = false;
+  })
+}
+function complementTag(editableLabel: VEditableLabel) {
+  _this.$components.complement.open(editableLabel, props.petaTagInfos.filter((pti) => {
+    return pti.petaTag.id !== UNTAGGED_ID;
+  }).map((pti) => {
+    return pti.petaTag.name;
+  }));
+}
+function tagMenu(event: PointerEvent, tag: PetaTag) {
+  _this.$components.contextMenu.open([
+    {
+      label: _this.$t("browser.property.tagMenu.remove", [tag.name]),
+      click: () => {
+        removeTag(tag);
+      }
+    }
+  ], vec2FromPointerEvent(event));
+}
+function selectTag(tag: PetaTag) {
+  emit("selectTag", tag);
+}
+async function fetchPetaTags() {
+  fetchingTags.value = true;
+  if (noImage.value) {
+    sharedPetaTags.value = [];
+    fetchingTags.value = false;
+    return;
+  }
+  const result = await API.send("getPetaTagIdsByPetaImageIds", props.petaImages.map((petaImage) => petaImage.id));
+  sharedPetaTags.value = props.petaTagInfos.filter((pti) => result.find((id) => id === pti.petaTag.id)).map((pi) => pi.petaTag);
+  fetchingTags.value = false;
+}
+const singlePetaImageInfo = computed(() => {
+  if (props.petaImages.length === 1) {
+    const petaImage = props.petaImages[0]!;
+    const all = petaImage.palette.reduce((p, c) => {
+      return p + c.population;
+    }, 0);
+    return {
+      name: petaImage.name,
+      petaImage: petaImage,
+      fileDate: dateFormat(petaImage.fileDate, "yyyy/mm/dd hh:MM:ss"),
+      addDate: dateFormat(petaImage.addDate, "yyyy/mm/dd hh:MM:ss"),
+      palette: petaImage.palette.map((color, i) => {
+        return {
+          color: `rgb(${color.r}, ${color.g}, ${color.b})`,
+          population: color.population / all,
+          id: i
+        };
+      })
+    }
+  }
+  return undefined;
+});
+const noImage = computed(() => {
+  return props.petaImages.length === 0;
+});
+const nsfw = computed(() => {
+  let nsfw = false;
+  let same = true;
+  props.petaImages.forEach((pi, i) => {
+    if (i === 0) {
+      nsfw = pi.nsfw;
+      return;
+    }
+    if (nsfw != pi.nsfw) {
+      same = false;
+    }
+  });
+  if (same) {
+    return nsfw;
+  }
+  return undefined;
+});
+watch(() => props.petaImages, () => {
+  fetchPetaTags();
+  console.log("petaTags");
+  if (singlePetaImageInfo.value) {
+    note.value = singlePetaImageInfo.value.petaImage.note;
+    nextTick(() => {
+      resizeNote();
+    })
+  } else {
+    note.value = "";
+  }
+});
+watch(() => props.petaTagInfos, () => {
+  fetchPetaTags();
+});
 </script>
 
 <style lang="scss" scoped>
