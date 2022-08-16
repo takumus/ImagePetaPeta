494b0b7b96ee15aa619d47f2542a838e95ef89e2
takumus <me@takumus.io>
2022-08-17 02:21:11 +0900

    Tile完了。

--- src/rendererProcess/components/browser/tile/VTile.vue
+++ src/rendererProcess/components/browser/tile/VTile.vue
@@ -13,7 +13,7 @@
         @dblclick="dblclick($event)"
         draggable="true"
         :class="{
-          'selected-image': tile.petaImage._selected
+          'selected-image': tile.petaImage?._selected
         }"
       >
         <t-nsfw v-if="showNSFW">
@@ -52,7 +52,7 @@
         </t-tag>
       </t-tags>
       <t-selected
-        v-show="tile.petaImage._selected"
+        v-show="tile.petaImage?._selected"
       >
         <t-icon>
           ✔
@@ -65,10 +65,9 @@
   </t-tile-root>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 // Vue
-import { Options, Vue } from "vue-class-component";
-import { Prop, Ref, Watch } from "vue-property-decorator";
+import { computed, nextTick, ref, watch, getCurrentInstance, onMounted, onUnmounted } from "vue";
 // Others
 import { Vec2, vec2FromPointerEvent } from "@/commons/utils/vec2";
 import { getImageURL } from "@/rendererProcess/utils/imageURL";
@@ -81,177 +80,159 @@ import { PetaTag } from "@/commons/datas/petaTag";
 import { PetaTagInfo } from "@/commons/datas/petaTagInfo";
 import { logChunk } from "@/rendererProcess/utils/rendererLogger";
 import { BROWSER_FETCH_TAGS_DELAY, BROWSER_FETCH_TAGS_DELAY_RANDOM, BROWSER_LOAD_ORIGINAL_DELAY, BROWSER_LOAD_ORIGINAL_DELAY_RANDOM, PLACEHOLDER_SIZE } from "@/commons/defines";
-@Options({
-  components: {
-  },
-  emits: ["select", "menu", "drag", "dblclick"]
-})
-export default class VTile extends Vue {
-  @Prop()
-  tile!: Tile;
-  @Prop()
-  original = false;
-  @Prop()
-  petaTagInfos!: PetaTagInfo[];
-  @Prop()
-  parentAreaMinY!: number;
-  @Prop()
-  parentAreaMaxY!: number;
-  imageURL = "";
-  pressing = false;
-  loadingImage = true;
-  loadingTags = true;
-  click: ClickChecker = new ClickChecker();
-  loadOriginalTimeoutHandler = -1;
-  fetchTagsTimeoutHandler = -1;
-  loadedOriginal = false;
-  mounted() {
-    this.updateContent(true);
-  }
-  unmounted() {
-    window.removeEventListener("pointermove", this.pointermove);
-    window.removeEventListener("pointerup", this.pointerup);
-    window.clearTimeout(this.loadOriginalTimeoutHandler);
-    window.clearTimeout(this.fetchTagsTimeoutHandler);
-  }
-  dragstart(event: PointerEvent) {
-    event.preventDefault();
-    this.$emit("drag", this.tile.petaImage);
-  }
-  pointerdown(event: PointerEvent) {
-    this.click.down(new Vec2(event.clientX, event.clientY));
-    window.addEventListener("pointermove", this.pointermove);
-    window.addEventListener("pointerup", this.pointerup);
-    switch (event.button) {
-      case MouseButton.LEFT: {
-        this.pressing = true;
-        break;
-      }
-    }
-    // API.send("startDrag", this.tile.petaImage);
-  }
-  pointermove(event: PointerEvent) {
-    if (!this.pressing) return;
-    this.click.move(new Vec2(event.clientX, event.clientY));
-    if (!this.click.isClick) {
-      // const img = (event as Event).target as HTMLElement;
-      // const elementRect = img.getBoundingClientRect();
-      // const position = new Vec2(
-      //   elementRect.x + elementRect.width / 2,
-      //   elementRect.y + elementRect.height / 2
-      // );
-      // this.$emit("add", this.tile, vec2FromPointerEvent(event), position);
-      this.pressing = false;
-    }
-  }
-  pointerup(event: PointerEvent) {
-    window.removeEventListener("pointermove", this.pointermove);
-    window.removeEventListener("pointerup", this.pointerup);
-    this.pressing = false;
-    if (this.click.isClick) {
-      switch(event.button) {
-        case MouseButton.LEFT:
-          this.$emit("select", this.tile);
-          break;
-        case MouseButton.RIGHT:
-          this.$emit("menu", this.tile, vec2FromPointerEvent(event));
-          break;
-      }
-    }
-  }
-  dblclick(event: PointerEvent) {
-    this.$emit("dblclick", this.tile.petaImage);
-  }
-  loaded() {
-    this.loadingImage = false;
-  }
-  get showNSFW() {
-    if (this.tile.petaImage === undefined) {
-      return false;
-    }
-    return this.tile.petaImage.nsfw && !this.$nsfw.showNSFW;
-  }
-  get placeholderColor() {
-    if (this.tile.petaImage === undefined) {
-      return "#ffffff";
-    }
-    const petaColor = this.tile.petaImage.palette[0];
-    if (petaColor) {
-      return `rgb(${petaColor.r}, ${petaColor.g}, ${petaColor.b})`;
-    }
-    return "#ffffff";
-  }
-  myPetaTags: PetaTag[] = [];
-  async fetchPetaTags() {
-    this.myPetaTags = [];
-    if (!this.$settings.showTagsOnTile) {
-      return;
-    }
-    if (this.tile.petaImage === undefined) {
-      return;
-    }
-    const result = await API.send("getPetaTagIdsByPetaImageIds", [this.tile.petaImage.id]);
-    this.myPetaTags = this.petaTagInfos
-    .filter((petaTagInfo) => result.find((id) => id === petaTagInfo.petaTag.id))
-    .map((pti) => pti.petaTag);
-    this.loadingTags = false;
-  }
-  updateContent(tags = false) {
-    if (tags) {
-      window.clearTimeout(this.fetchTagsTimeoutHandler);
-    }
-    window.clearTimeout(this.loadOriginalTimeoutHandler);
-    if (this.tile.visible) {
-      if (tags) {
-        this.fetchTagsTimeoutHandler = window.setTimeout(() => {
-          this.fetchPetaTags();
-        }, Math.random() * BROWSER_FETCH_TAGS_DELAY_RANDOM + BROWSER_FETCH_TAGS_DELAY);
-      }
-      if (!this.loadedOriginal) {
-        this.imageURL = getImageURL(this.tile.petaImage, ImageType.THUMBNAIL);
-        if (this.original) {
-          this.loadOriginalTimeoutHandler = window.setTimeout(() => {
-            this.imageURL = getImageURL(this.tile.petaImage, ImageType.ORIGINAL);
-            this.loadedOriginal = true;
-          }, Math.random() * BROWSER_LOAD_ORIGINAL_DELAY_RANDOM + BROWSER_LOAD_ORIGINAL_DELAY);
-        }
-      }
-    } else {
-      this.loadedOriginal = false;
-    }
-  }
-  get visible() {
-    return this.tile.visible;
-  }
-  @Watch("visible")
-  changeVisible() {
-    this.updateContent(true);
-  }
-  @Watch("tile.width")
-  changeTileWidth() {
-    this.updateContent();
-  }
-  @Watch("original")
-  changeOriginal() {
-    this.updateContent();
-  }
-  @Watch("parentAreaMinY")
-  changeParentAreaMinY() {
-    this.updateContent();
-  }
-  @Watch("parentAreaMaxY")
-  changeParentAreaMaxY() {
-    this.updateContent();
-  }
-  @Watch("petaTagInfos")
-  changeFetchTags() {
-    this.updateContent(true);
-  }
-  @Watch("$settings.showTagsOnTile")
-  changeShowTagsOnTile() {
-    this.updateContent(true);
+import { PetaImage } from "@/commons/datas/petaImage";
+
+const emit = defineEmits<{
+  (e: "select", tile: Tile): void
+  (e: "menu", tile: Tile, position: Vec2): void,
+  (e: "drag", petaImage: PetaImage): void,
+  (e: "dblclick", petaImage: PetaImage): void
+}>();
+const props = defineProps<{
+  tile: Tile,
+  original: boolean,
+  petaTagInfos: PetaTagInfo[],
+  parentAreaMinY: number,
+  parentAreaMaxY: number
+}>();
+const _this = getCurrentInstance()!.proxy!;
+const imageURL = ref("");
+const loadingImage = ref(true);
+const loadingTags = ref(true);
+const loadedOriginal = ref(false);
+const myPetaTags = ref<PetaTag[]>([]);
+const click: ClickChecker = new ClickChecker();
+let loadOriginalTimeoutHandler = -1;
+let fetchTagsTimeoutHandler = -1;
+let pressing = false;
+onMounted(() => {
+  updateContent(true);
+});
+onUnmounted(() => {
+  window.removeEventListener("pointermove", pointermove);
+  window.removeEventListener("pointerup", pointerup);
+  window.clearTimeout(loadOriginalTimeoutHandler);
+  window.clearTimeout(fetchTagsTimeoutHandler);
+});
+function dragstart(event: PointerEvent) {
+  event.preventDefault();
+  if (props.tile.petaImage) {
+    emit("drag", props.tile.petaImage);
   }
 }
+function pointerdown(event: PointerEvent) {
+  click.down(new Vec2(event.clientX, event.clientY));
+  window.addEventListener("pointermove", pointermove);
+  window.addEventListener("pointerup", pointerup);
+  switch (event.button) {
+    case MouseButton.LEFT: {
+      pressing = true;
+      break;
+    }
+  }
+}
+function pointermove(event: PointerEvent) {
+  if (!pressing) return;
+  click.move(new Vec2(event.clientX, event.clientY));
+  if (!click.isClick) {
+    pressing = false;
+  }
+}
+function pointerup(event: PointerEvent) {
+  window.removeEventListener("pointermove", pointermove);
+  window.removeEventListener("pointerup", pointerup);
+  pressing = false;
+  if (click.isClick) {
+    switch(event.button) {
+      case MouseButton.LEFT:
+        emit("select", props.tile);
+        break;
+      case MouseButton.RIGHT:
+        emit("menu", props.tile, vec2FromPointerEvent(event));
+        break;
+    }
+  }
+}
+function dblclick(event: PointerEvent) {
+  if (props.tile.petaImage) {
+    emit("dblclick", props.tile.petaImage);
+  }
+}
+function loaded() {
+  loadingImage.value = false;
+}
+const showNSFW = computed(() => {
+  if (props.tile.petaImage === undefined) {
+    return false;
+  }
+  return props.tile.petaImage.nsfw && !_this.$nsfw.showNSFW;
+});
+const placeholderColor = computed(() => {
+  if (props.tile.petaImage === undefined) {
+    return "#ffffff";
+  }
+  const petaColor = props.tile.petaImage.palette[0];
+  if (petaColor) {
+    return `rgb(${petaColor.r}, ${petaColor.g}, ${petaColor.b})`;
+  }
+  return "#ffffff";
+});
+async function fetchPetaTags() {
+  myPetaTags.value = [];
+  if (!_this.$settings.showTagsOnTile) {
+    return;
+  }
+  if (props.tile.petaImage === undefined) {
+    return;
+  }
+  const result = await API.send("getPetaTagIdsByPetaImageIds", [props.tile.petaImage.id]);
+  myPetaTags.value = props.petaTagInfos
+  .filter((petaTagInfo) => result.find((id) => id === petaTagInfo.petaTag.id))
+  .map((pti) => pti.petaTag);
+  loadingTags.value = false;
+}
+function updateContent(tags = false) {
+  if (tags) {
+    window.clearTimeout(fetchTagsTimeoutHandler);
+  }
+  window.clearTimeout(loadOriginalTimeoutHandler);
+  if (props.tile.visible) {
+    if (tags) {
+      fetchTagsTimeoutHandler = window.setTimeout(() => {
+        fetchPetaTags();
+      }, Math.random() * BROWSER_FETCH_TAGS_DELAY_RANDOM + BROWSER_FETCH_TAGS_DELAY);
+    }
+    if (!loadedOriginal.value) {
+      imageURL.value = getImageURL(props.tile.petaImage, ImageType.THUMBNAIL);
+      if (props.original) {
+        loadOriginalTimeoutHandler = window.setTimeout(() => {
+          imageURL.value = getImageURL(props.tile.petaImage, ImageType.ORIGINAL);
+          loadedOriginal.value = true;
+        }, Math.random() * BROWSER_LOAD_ORIGINAL_DELAY_RANDOM + BROWSER_LOAD_ORIGINAL_DELAY);
+      }
+    }
+  } else {
+    loadedOriginal.value = false;
+  }
+}
+const visible = computed(() => {
+  return props.tile.visible;
+});
+watch([
+  () => props.parentAreaMinY,
+  () => props.parentAreaMaxY,
+  () => props.original,
+  () => props.tile.width
+], () => {
+  updateContent();
+});
+watch([
+  visible,
+  () => props.petaTagInfos,
+  () => _this.$settings.showTagsOnTile,
+], () => {
+  updateContent(true);
+});
 </script>
 
 <style lang="scss" scoped>
