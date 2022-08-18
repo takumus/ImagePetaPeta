c0193b20f95ba7527ade9ac6e9bd169daf065e92
takumus <me@takumus.io>
2022-08-19 04:44:32 +0900

    Update task.js

--- scripts/task.js
+++ scripts/task.js
@@ -1,13 +1,13 @@
 const chalk = require("chalk");
 module.exports = async function (name, task) {
-  console.log(chalk.green.bold(`\nTask ${name} begin`));
+  console.log(chalk.green.bold(`\nTask "${name}" begin`));
   try {
     await task((...args) => {
       console.log(chalk.yellow(args.join("")));
     });
-    console.log(chalk.green.bold(`Task ${name} complete\n`));
+    console.log(chalk.green.bold(`Task "${name}" complete\n`));
   } catch (error) {
-    console.error(chalk.red.bold(`Task ${name} failed\n${error}\n`));
+    console.error(chalk.red.bold(`Task "${name}" failed\n${error}\n`));
     process.exit(1);
   }
 };
