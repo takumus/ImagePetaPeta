09f312a04a53926b1f2b568da8b2d6064e33abf3
takumus <me@takumus.io>
2022-08-19 01:10:45 +0900

    イラン

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    'prettier'
-  ],
-  parser: 'vue-eslint-parser',
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/typescript/recommended", "prettier"],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 0,
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": 0,
     // "@typescript-eslint/strict-boolean-expressions": ["error"]
   },
   globals: {
     defineProps: "readonly",
     defineEmits: "readonly",
     defineExpose: "readonly",
-    withDefaults: "readonly"
-  }
-}
+    withDefaults: "readonly",
+  },
+};
