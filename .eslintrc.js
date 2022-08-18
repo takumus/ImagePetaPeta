7699373973bc7b75a62de849cbd1095b5ca83d9c
takumus <me@takumus.io>
2022-08-19 01:44:57 +0900

    整理

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": 0,
-    // "@typescript-eslint/strict-boolean-expressions": ["error"]
   },
   globals: {
     defineProps: "readonly",
