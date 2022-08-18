b5a4e1032a7daf7700145023c08a243ab74e9a95
takumus <me@takumus.io>
2022-08-19 04:22:22 +0900

    seiri

--- scripts/generateIcons.js
+++ scripts/generateIcons.js
@@ -4,15 +4,7 @@ const sharp = require("sharp");
 
 (async () => {
   console.log("generate icons");
-  const ico = new IconIco();
-  const sizes = [16, 32, 48, 64, 128, 256];
-  for (let i = 0; i < sizes.length; i++) {
-    const size = sizes[i];
-    const buf = await sharp("./rawAssets/icon/icon.png").resize(size).toBuffer();
-    ico.addFromPng(buf, false, false);
-  }
-  fs.writeFileSync("./build/WindowsIcon.ico", ico.encode());
-  console.log(`./build/WindowsIcon.ico`);
+  await exportIcon("./rawAssets/icon/icon.png", "./build/WindowsIcon.ico", [16, 32, 48, 64, 128, 256]);
   await exportImage("./rawAssets/icon/icon.png", "./build/Square44x44Logo.png", 44);
   await exportImage("./rawAssets/icon/icon.png", "./build/Square150x150Logo.png", 150);
   await exportImage("./rawAssets/icon/icon.png", "./build/StoreLogo.png", 50);
@@ -22,5 +14,15 @@ const sharp = require("sharp");
 
 async function exportImage(from, to, size) {
   await sharp(from).resize(size).toFile(to);
+  console.log(to);
+}
+async function exportIcon(from, to, sizes) {
+  const ico = new IconIco();
+  for (let i = 0; i < sizes.length; i++) {
+    const size = sizes[i];
+    const buf = await sharp(from).resize(size).toBuffer();
+    ico.addFromPng(buf, false, false);
+  }
+  fs.writeFileSync(to, ico.encode());
   console.log(to);
 }
