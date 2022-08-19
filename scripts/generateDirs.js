8e5bd165d8b5838b713ad7f7d65438b09f06dc90
takumus <me@takumus.io>
2022-08-20 01:39:31 +0900

    rimrafこっちでやる。

--- scripts/generateDirs.js
+++ scripts/generateDirs.js
@@ -1,6 +1,8 @@
 const task = require("./@task");
 const fs = require("fs");
+const rimraf = require("rimraf");
 task("generate dirs", async () => {
+  rimraf.sync("./dist/electron");
   fs.mkdir("./dist", (err) => err);
   fs.mkdir("./dist/resources", (err) => err);
   fs.mkdir("./dist/test", (err) => err);
