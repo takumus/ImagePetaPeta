bc6c6290a6ba1332af23cbf1bc55fe3cda841976
takumus <me@takumus.io>
2022-09-09 04:28:35 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-const pages = require("./vue.config.pages.js");
+const pages = require("./vue.config.pages");
 const chainWebpackRendererProcess = require("./vue.config.webpack.renderer");
 const chainWebpackMainProcess = require("./vue.config.webpack.main");
 const electronBuilder = require("./vue.config.electron-builder");
