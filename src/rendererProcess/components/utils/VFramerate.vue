3fd0f33349206128cb210a92b239fd33a1b74525
takumus <me@takumus.io>
2022-10-30 23:55:22 +0900

    fps 左下。

--- src/rendererProcess/components/utils/VFramerate.vue
+++ src/rendererProcess/components/utils/VFramerate.vue
@@ -1,10 +1,12 @@
 <template>
-  <t-framerate-root-root> {{ fps }}fps </t-framerate-root-root>
+  <t-framerate-root v-if="settingsStore.state.value.showFPS"> {{ fps }}fps </t-framerate-root>
 </template>
 
 <script setup lang="ts">
+import { useSettingsStore } from "@/rendererProcess/stores/settingsStore";
 import { onMounted, ref } from "vue";
 const fps = ref(999);
+const settingsStore = useSettingsStore();
 let count = 0;
 const INTERVAL = 500;
 onMounted(() => {
@@ -21,10 +23,11 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
-t-framerate-root-root {
+t-framerate-root {
   z-index: 999;
   position: fixed;
-  right: 0px;
+  left: 0px;
   bottom: 0px;
+  margin: var(--px1);
 }
 </style>
