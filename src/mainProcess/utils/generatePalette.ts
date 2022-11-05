ae3e426c2e0fc218c2b79fefe47560f9bd7be75b
takumus <me@takumus.io>
2022-11-06 06:04:23 +0900

    色抽出を高速化＆最適化

--- src/mainProcess/utils/generatePalette.ts
+++ src/mainProcess/utils/generatePalette.ts
@@ -18,8 +18,9 @@ function createPixels(buffer: Buffer, pixelCount: number) {
 export function getSimplePalette(imageData: { buffer: Buffer; width: number; height: number }) {
   const palette = getPalette({
     sample: 1000,
-    mergeCIEDiff: 15,
+    beforeMergeCIEDiff: 10,
     fixColorCIEDiff: 8,
+    afterMergeCIEFiff: 15,
     ...imageData,
   });
   return palette;
@@ -27,8 +28,9 @@ export function getSimplePalette(imageData: { buffer: Buffer; width: number; hei
 export function getFullPalette(imageData: { buffer: Buffer; width: number; height: number }) {
   const palette = getPalette({
     sample: 10000,
-    mergeCIEDiff: 5,
+    beforeMergeCIEDiff: 5,
     fixColorCIEDiff: 4,
+    afterMergeCIEFiff: 4,
     ...imageData,
   });
   return palette;
@@ -38,25 +40,26 @@ export function getPalette(imageData: {
   width: number;
   height: number;
   sample: number;
-  mergeCIEDiff: number;
+  beforeMergeCIEDiff: number;
   fixColorCIEDiff: number;
+  afterMergeCIEFiff: number;
 }): PetaColor[] {
   const pixelCount = imageData.width * imageData.height;
   const pixels = createPixels(imageData.buffer, imageData.width * imageData.height);
   const qPalette = quantize(pixels, 256).palette();
-  const colors = qPalette.map((color): PetaColor => {
-    return {
+  const colors = qPalette.map(
+    (color): PetaColor => ({
       r: color[0],
       g: color[1],
       b: color[2],
       population: 0,
       positionSD: 0,
-    };
-  });
+    }),
+  );
   for (let i = 0; i < colors.length; i++) {
     for (let ii = i + 1; ii < colors.length; ii++) {
-      const cieDiff = ciede(colors[i]!, colors[ii]!);
-      if (cieDiff < imageData.mergeCIEDiff) {
+      const cieDiff = ciede(colors[i], colors[ii]);
+      if (cieDiff < imageData.beforeMergeCIEDiff) {
         colors.splice(ii, 1);
         ii--;
       }
@@ -66,18 +69,13 @@ export function getPalette(imageData: {
   if (resolution < 1) {
     resolution = 1;
   }
-  colors.map((color) => {
+  colors.forEach((color) => {
     const similars: { [key: string]: { count: number; color: [number, number, number] } } = {};
-    const positions: { x: number; y: number }[] = [];
     for (let i = 0; i < pixels.length; i += resolution) {
       const rc = pixels[i]!;
-      const y = Math.floor(i / imageData.width);
-      const x = i % imageData.width;
-      const cieDiff = ciede(rc, [color.r, color.g, color.b]);
-      if (cieDiff < imageData.fixColorCIEDiff) {
+      if (ciede(rc, color) < imageData.fixColorCIEDiff) {
         color.population++;
-        positions.push({ x, y });
-        const key = rc[0] + "," + rc[1] + "," + rc[2];
+        const key = rc.join(",");
         if (similars[key] === undefined) {
           similars[key] = {
             count: 1,
@@ -88,43 +86,31 @@ export function getPalette(imageData: {
         }
       }
     }
-    const similar = Object.values(similars).sort((a, b) => {
-      return b.count - a.count;
-    })[0];
+    const similar = Object.values(similars).sort((a, b) => b.count - a.count)[0];
     if (similar !== undefined) {
       color.r = similar.color[0];
       color.g = similar.color[1];
       color.b = similar.color[2];
-      const avgPos = positions.reduce(
-        (p, c) => {
-          return {
-            x: p.x + c.x,
-            y: p.y + c.y,
-          };
-        },
-        { x: 0, y: 0 },
-      );
-      avgPos.x /= color.population;
-      avgPos.y /= color.population;
-      const sdPos = positions.reduce(
-        (p, c) => {
-          return {
-            x: p.x + Math.pow(c.x - avgPos.x, 2),
-            y: p.y + Math.pow(c.y - avgPos.y, 2),
-          };
-        },
-        { x: 0, y: 0 },
-      );
-      sdPos.x /= color.population;
-      sdPos.y /= color.population;
-      color.positionSD = Math.floor(sdPos.x + sdPos.y);
     }
   });
-  return colors
+  const palette = colors
     .filter((color) => {
       return color.population > 0;
     })
     .sort((a, b) => {
       return b.population - a.population;
     });
+  for (let i = 0; i < palette.length; i++) {
+    for (let ii = i + 1; ii < palette.length; ii++) {
+      const cieDiff = ciede(palette[i], palette[ii]);
+      if (cieDiff < imageData.afterMergeCIEFiff) {
+        palette[i]!.population += palette[ii]!.population;
+        palette.splice(ii, 1);
+        ii--;
+      }
+    }
+  }
+  return palette.sort((a, b) => {
+    return b.population - a.population;
+  });
 }
