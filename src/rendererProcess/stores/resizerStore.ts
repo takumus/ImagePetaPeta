e5e8b2f6af97500b7859d8b0e242e2c63e6c647c
takumus <me@takumus.io>
2022-11-07 20:10:06 +0900

    強制エミット実装

--- src/rendererProcess/stores/resizerStore.ts
+++ src/rendererProcess/stores/resizerStore.ts
@@ -6,6 +6,7 @@ export function useResizerStore() {
   const eventEmitter = new EventEmitter() as TypedEventEmitter<{
     resize: (rect: DOMRectReadOnly) => void;
   }>;
+  let _element: Element | undefined;
   const resizer = new ResizeObserver((entries) => {
     const rect = entries[0]?.contentRect;
     if (rect) {
@@ -14,13 +15,24 @@ export function useResizerStore() {
   });
   onUnmounted(() => {
     resizer.disconnect();
+    eventEmitter.removeAllListeners();
   });
+  function forceEmit() {
+    if (_element !== undefined) {
+      eventEmitter.emit("resize", _element.getBoundingClientRect());
+    }
+  }
   return {
     on: eventEmitter.on.bind(eventEmitter),
-    observe: (element?: Element) => {
+    observe: (element?: Element, immidiately = true) => {
       if (element !== undefined) {
+        _element = element;
         resizer.observe(element);
+        if (immidiately) {
+          forceEmit();
+        }
       }
     },
+    forceEmit,
   };
 }
