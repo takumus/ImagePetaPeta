9bbe113b987da869b027d12281ec428aa32ae535
takumus <me@takumus.io>
2022-11-11 07:12:52 +0900

    上長削除

--- src/rendererProcess/ipc/index.ts
+++ src/rendererProcess/ipc/index.ts
@@ -1,29 +1,27 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MainFunctions } from "@/commons/api/mainFunctions";
 import { MainEvents } from "@/commons/api/mainEvents";
 import { IpcRendererEvent } from "electron/main";
 import deepcopy from "deepcopy";
 import { IPC_GLOBAL_NAME } from "@/commons/defines";
-interface IPC {
-  send<U extends keyof MainFunctions>(
-    event: U,
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+const WINDOW = window as any;
+export const IPC = {
+  send: <U extends keyof MainFunctions>(
+    e: U,
     ...args: Parameters<MainFunctions[U]>
-  ): ReturnType<MainFunctions[U]>;
-  on<U extends keyof MainEvents>(
-    event: U,
-    callback: (event: IpcRendererEvent, ...args: Parameters<MainEvents[U]>) => void,
-  ): string;
-  off(id: string): void;
-}
-export const IPC: IPC = {
-  send: (e: any, ...args: any[]) => {
-    args = args.map((arg) => (arg === undefined ? undefined : arg === null ? null : deepcopy(arg)));
-    return (window as any)[IPC_GLOBAL_NAME].send(e, ...args);
+  ): ReturnType<MainFunctions[U]> => {
+    return WINDOW[IPC_GLOBAL_NAME].send(
+      e,
+      ...args.map((arg) => (arg === undefined ? undefined : arg === null ? null : deepcopy(arg))),
+    );
   },
-  on: (e: any, cb: any) => {
-    return (window as any)[IPC_GLOBAL_NAME].on(e, cb);
+  on: <U extends keyof MainEvents>(
+    e: U,
+    cb: (event: IpcRendererEvent, ...args: Parameters<MainEvents[U]>) => void,
+  ): string => {
+    return WINDOW[IPC_GLOBAL_NAME].on(e, cb);
   },
-  off: (id: any) => {
-    return (window as any)[IPC_GLOBAL_NAME].off(id);
+  off: (id: string): void => {
+    return WINDOW[IPC_GLOBAL_NAME].off(id);
   },
 };
