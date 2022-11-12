cbc007ca52f3a52fcec397c23750e686256b0d8b
takumus <me@takumus.io>
2022-11-13 04:08:47 +0900

    タグソート仮

--- src/rendererProcess/components/browser/tags/VTags.vue
+++ src/rendererProcess/components/browser/tags/VTags.vue
@@ -1,26 +1,41 @@
 <template>
   <t-tags-root>
-    <t-tags>
+    <t-tags-top>
       <VTagCell
         @click="selectPetaTag()"
         :selected="selectedAll"
         :readonly="true"
         :look="`${t('browser.all')}(${petaImagesArray.length})`"
       />
-    </t-tags>
+    </t-tags-top>
     <t-tags>
       <VTagCell
         v-for="c in browserTags"
         :key="c.petaTag.id"
-        :selected="c.selected"
+        :selected="(c.selected && draggingPetaTag === undefined) || draggingPetaTag === c.petaTag"
         @click="selectPetaTag(c.petaTag)"
+        @mousedown="startDrag($event, c.petaTag)"
         :readonly="c.readonly"
         :value="c.petaTag.name"
         :look="`${c.petaTag.name}`"
+        :ref="(element) => setVTagCellRef(element as any as VTagCellInstance, c.petaTag.id)"
         @update:value="(name) => changeTag(c.petaTag, name)"
         @contextmenu="tagMenu($event, c)"
+        :style="{
+          order: orders[c.petaTag.id] ?? browserTags.length,
+        }"
       />
     </t-tags>
+    <t-tag-dragging v-if="draggingPetaTag">
+      <VTagCell
+        :key="draggingPetaTag.id"
+        :selected="true"
+        :readonly="true"
+        :value="draggingPetaTag.name"
+        :look="`${draggingPetaTag.name}`"
+        ref="draggingTagCell"
+      />
+    </t-tag-dragging>
     <t-tag-add>
       <t-tag>
         <VTextarea
@@ -39,13 +54,13 @@
 
 <script setup lang="ts">
 // Vue
-import { computed } from "vue";
+import { computed, nextTick, onBeforeUpdate, ref, watch } from "vue";
 import { PetaImage } from "@/commons/datas/petaImage";
 import { PetaTag } from "@/commons/datas/petaTag";
 import { BrowserTag } from "@/rendererProcess/components/browser/browserTag";
 import { UpdateMode } from "@/commons/datas/updateMode";
 import { Keyboards } from "@/rendererProcess/utils/keyboards";
-import { vec2FromPointerEvent } from "@/commons/utils/vec2";
+import { Vec2, vec2FromPointerEvent } from "@/commons/utils/vec2";
 import { UNTAGGED_ID } from "@/commons/defines";
 import { useTextsStore } from "@/rendererProcess/stores/textsStore";
 import { useI18n } from "vue-i18n";
@@ -53,6 +68,7 @@ import { useComponentsStore } from "@/rendererProcess/stores/componentsStore";
 import VTextarea from "@/rendererProcess/components/utils/VTextarea.vue";
 import { usePetaTagsStore } from "@/rendererProcess/stores/petaTagsStore";
 import VTagCell from "@/rendererProcess/components/browser/tags/VTagCell.vue";
+type VTagCellInstance = InstanceType<typeof VTagCell>;
 const emit = defineEmits<{
   (e: "update:selectedPetaTagIds", selectedPetaTagIds: string[]): void;
 }>();
@@ -64,6 +80,138 @@ const textsStore = useTextsStore();
 const components = useComponentsStore();
 const petaTagsStore = usePetaTagsStore();
 const { t } = useI18n();
+const vTagCells = ref<{ [key: string]: VTagCellInstance }>({});
+const draggingPetaTag = ref<PetaTag>();
+const draggingTagCell = ref<VTagCellInstance>();
+const startDragOffset = ref(new Vec2());
+const orders = ref<{ [key: string]: number }>({});
+onBeforeUpdate(() => {
+  vTagCells.value = {};
+});
+function setVTagCellRef(element: VTagCellInstance, id: string) {
+  vTagCells.value[id] = element;
+}
+function startDrag(event: PointerEvent, petaTag: PetaTag) {
+  const element = event.currentTarget as HTMLElement;
+  if (element === undefined) {
+    return;
+  }
+  const rect = element.getBoundingClientRect();
+  startDragOffset.value = vec2FromPointerEvent(event);
+  draggingPetaTag.value = petaTag;
+  nextTick(() => {
+    const draggingCellStyle = draggingTagCell.value?.$el.style as CSSStyleDeclaration;
+    if (draggingCellStyle === undefined) {
+      return;
+    }
+    draggingCellStyle.width = rect.width + "px";
+    draggingCellStyle.height = rect.height + "px";
+    const mouse = vec2FromPointerEvent(event);
+    const style = draggingTagCell.value?.$el.style as CSSStyleDeclaration;
+    style.transform = `translate(${mouse.x}px, ${mouse.y}px)`;
+    const prevOrders = JSON.stringify(orders.value);
+    function pointermove(event: PointerEvent) {
+      const rect = element.getBoundingClientRect();
+      const mouse = vec2FromPointerEvent(event);
+      style.transform = `translate(${mouse.x}px, ${mouse.y}px)`;
+      [
+        ...Object.keys(vTagCells.value)
+          .map((id) => ({
+            id,
+            element: vTagCells.value[id]?.$el as HTMLElement,
+          }))
+          .filter((cell) => cell.id !== petaTag.id)
+          .map((cell) => {
+            const rect = cell.element.getBoundingClientRect();
+            return {
+              me: false,
+              x: rect.x,
+              y: rect.y,
+              width: rect.width,
+              height: rect.height,
+              id: cell.id,
+            };
+          }),
+        {
+          me: true,
+          x: mouse.x,
+          y: mouse.y,
+          width: rect.width,
+          height: rect.height,
+          id: petaTag.id,
+        },
+      ]
+        .sort((a, b) => {
+          if (a.me) {
+            if (a.y > b.y && a.y < b.y + b.height) {
+              if (a.x > b.x && a.x < b.x + b.width && rect.y != b.y) {
+                return -1;
+              }
+              if (a.x < b.x + b.width / 2) {
+                return -1;
+              } else {
+                return 1;
+              }
+            }
+          } else if (b.me) {
+            if (b.y > a.y && b.y < a.y + a.height) {
+              if (b.x > a.x && b.x < a.x + a.width && rect.y != b.y) {
+                return -1;
+              }
+              if (a.x < b.x + b.width / 2) {
+                return -1;
+              } else {
+                return 1;
+              }
+            }
+          }
+          if (a.y < b.y) {
+            return -1;
+          } else if (a.y > b.y) {
+            return 1;
+          } else {
+            if (a.x < b.x) {
+              return -1;
+            } else {
+              return 1;
+            }
+          }
+        })
+        .forEach((result, i) => {
+          orders.value[result.id] = i;
+        });
+      Object.keys(orders.value)
+        .map((id) => ({
+          order: orders.value[id] ?? 0,
+          id,
+        }))
+        .sort((a, b) => a.order - b.order)
+        .forEach((o, index) => {
+          orders.value[o.id] = index;
+        });
+    }
+    function pointerup() {
+      window.removeEventListener("pointermove", pointermove);
+      window.removeEventListener("pointerup", pointerup);
+      const afterOrders = JSON.stringify(orders.value);
+      draggingPetaTag.value = undefined;
+      if (prevOrders !== afterOrders) {
+        petaTagsStore.updatePetaTags(
+          Object.values(petaTagsStore.state.petaTags.value).map((petaTag) => ({
+            type: "petaTag",
+            petaTag: {
+              ...petaTag,
+              index: orders.value[petaTag.id] ?? 0,
+            },
+          })),
+          UpdateMode.UPDATE,
+        );
+      }
+    }
+    window.addEventListener("pointermove", pointermove);
+    window.addEventListener("pointerup", pointerup);
+  });
+}
 function tagMenu(event: PointerEvent | MouseEvent, tag: BrowserTag) {
   if (tag.readonly) {
     return;
@@ -160,6 +308,15 @@ const browserTags = computed((): BrowserTag[] => {
   });
   return browserTags;
 });
+watch(
+  petaTagsStore.state.petaTags,
+  () => {
+    petaTagsStore.state.petaTags.value.forEach((petaTag) => {
+      orders.value[petaTag.id] = petaTag.index;
+    });
+  },
+  { immediate: true },
+);
 const selectedAll = computed(() => {
   return props.selectedPetaTagIds.length === 0;
 });
@@ -177,17 +334,34 @@ t-tags-root {
       padding: var(--px-1);
     }
   }
-  > t-tags {
+  > t-tags-top {
     outline: none;
     padding: var(--px-1) var(--px-1) 0px 0px;
     width: 100%;
     text-align: left;
-    overflow-y: auto;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
     gap: var(--px-1);
+  }
+  > t-tags {
+    outline: none;
+    padding: var(--px-1) var(--px-1) 0px 0px;
+    width: 100%;
+    text-align: left;
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: var(--px-1);
+  }
+  > t-tag-dragging {
+    top: 0px;
+    left: 0px;
+    position: fixed;
+    visibility: hidden;
   }
 }
 </style>
