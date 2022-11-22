fc53fe1a76cde61cf01c73106573053956fbcdcd
takumus <me@takumus.io>
2022-11-22 21:21:45 +0900

    anyいらない

--- src/main/libs/di.ts
+++ src/main/libs/di.ts
@@ -1,5 +1,4 @@
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const map: { [key: string]: any } = {};
+const map: { [key: string]: unknown } = {};
 
 interface Key<T> {
   key: string;
@@ -17,14 +16,13 @@ export function provide<T>(key: Key<T>, instance: T) {
   map[key.key] = instance;
 }
 export function inject<T>(key: Key<T>): T {
-  const instance = map[key.key];
+  const instance = map[key.key] as T;
   if (instance === undefined) {
     throw new Error(`DI: Could not inject ${key.key}`);
   }
   return instance;
 }
 export function createUseFunction<T>(key: Key<T>) {
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   let instance: T | undefined = undefined;
   return (cache = true) => {
     if (instance === undefined || !cache) {
