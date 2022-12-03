71b2f7b3d0623144f24c87584fbc46c1d7d1383e
takumus <me@takumus.io>
2022-12-04 08:00:12 +0900

    試しにシークバーテスト。

---
+++ src/renderer/components/commons/playbackController/VPlaybackController.vue
@@ -0,0 +1,89 @@
+<template>
+  <t-playback-controller-root>
+    <button @click="play">play</button>
+    <button @click="pause">pause</button>
+    <input
+      type="range"
+      :max="duration"
+      v-model="currentTimeModel"
+      @pointerdown="seeking = true"
+      @change="seeking = false" />
+    <input type="range" :max="1000" v-model="currentVolumeModel" />
+  </t-playback-controller-root>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
+
+import { PFileObjectContent } from "@/renderer/utils/pFileObject/pFileObjectContent";
+import { PVideoFileObjectContent } from "@/renderer/utils/pFileObject/video";
+
+const props = defineProps<{
+  pFileObjectContent: PFileObjectContent<unknown>;
+}>();
+const duration = ref(0);
+const currentTime = ref(0);
+const currentVolumeTime = ref(0);
+const seeking = ref(false);
+function play() {
+  if (props.pFileObjectContent instanceof PVideoFileObjectContent) {
+    props.pFileObjectContent.play();
+  }
+}
+function pause() {
+  if (props.pFileObjectContent instanceof PVideoFileObjectContent) {
+    props.pFileObjectContent.pause();
+  }
+}
+const currentTimeModel = computed<number>({
+  get() {
+    return currentTime.value;
+  },
+  set(value) {
+    if (props.pFileObjectContent instanceof PVideoFileObjectContent) {
+      const video = props.pFileObjectContent;
+      video.setCurrentTime(value / 1000);
+    }
+  },
+});
+const currentVolumeModel = computed<number>({
+  get() {
+    return currentVolumeTime.value;
+  },
+  set(value) {
+    if (props.pFileObjectContent instanceof PVideoFileObjectContent) {
+      const video = props.pFileObjectContent;
+      video.setVolume(value / 1000);
+    }
+  },
+});
+watch(
+  () => props.pFileObjectContent,
+  () => {
+    console.log("ok");
+    if (props.pFileObjectContent instanceof PVideoFileObjectContent) {
+      const video = props.pFileObjectContent;
+      duration.value = video.getDuration() * 1000;
+      video.event.on("updateRenderer", () => {
+        if (seeking.value) {
+          return;
+        }
+        currentTime.value = video.getCurrentTime() * 1000;
+        currentVolumeTime.value = video.getVolume() * 1000;
+      });
+    }
+  },
+  { immediate: true },
+);
+</script>
+
+<style lang="scss" scoped>
+t-playback-controller-root {
+  display: block;
+  width: 100%;
+  height: 100%;
+  > input {
+    width: 100%;
+  }
+}
+</style>
