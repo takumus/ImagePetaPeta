e1ab12675211e173d75e7f2cb5255933809abcd1
takumus <me@takumus.io>
2022-12-04 22:02:19 +0900

    プレイアブルをまとめた抽象クラス

---
+++ src/renderer/utils/pFileObject/pPlayableFileObjectContainer.ts
@@ -0,0 +1,15 @@
+import { EventMap } from "typed-emitter";
+
+import { PFileObjectContent } from "@/renderer/utils/pFileObject/pFileObjectContent";
+
+export abstract class PPlayableFileObjectContent<
+  T extends EventMap | unknown,
+> extends PFileObjectContent<T> {
+  abstract play(): void;
+  abstract pause(): void;
+  abstract getPaused(): boolean;
+  abstract getDuration(): number;
+  abstract getCurrentTime(): number;
+  abstract getSeekable(): boolean;
+  abstract setCurrentTime(currentTime: number): void;
+}
