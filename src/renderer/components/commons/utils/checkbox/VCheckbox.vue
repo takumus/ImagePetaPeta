659a59fcc89fa2b95b61333298202bbc49558c47
takumus <me@takumus.io>
2022-12-24 15:07:42 +0900

    色修正

--- src/renderer/components/commons/utils/checkbox/VCheckbox.vue
+++ src/renderer/components/commons/utils/checkbox/VCheckbox.vue
@@ -29,26 +29,27 @@ t-checkbox-root {
     height: 100%;
     aspect-ratio: 2;
     border-radius: 999px;
-    // border: solid var(--px-0) var(--color-border);
     position: relative;
-    background-color: var(--color-2);
+    background-color: var(--color-checkbox-false-background);
     overflow: hidden;
     > t-circle {
       display: block;
       border-radius: 999px;
-      background-color: var(--color-border);
+      background-color: var(--color-checkbox-false-circle);
       height: 100%;
       aspect-ratio: 1;
-      transform: scale(1);
+      transform: scale(0.7);
       position: relative;
       transition-duration: 100ms;
-      transition-property: left;
+      transition-property: left, background-color;
       left: 0%;
+      box-shadow: 0px 0.5px 2px rgba(0, 0, 0, 0.2);
     }
     &.checked {
+      background-color: var(--color-checkbox-true-background);
       > t-circle {
         left: 50%;
-        background-color: var(--color-border);
+        background-color: var(--color-checkbox-true-circle);
       }
     }
   }
