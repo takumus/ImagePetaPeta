f2d08cebc2223055d0e48ee382012c1c4d9421a9
takumus <me@takumus.io>
2023-03-16 23:49:27 +0900

    HMSを綺麗に

--- src/commons/utils/secondsToHMS.ts
+++ src/commons/utils/secondsToHMS.ts
@@ -1,9 +1,7 @@
 export function secondsToHMS(seconds: number) {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor((seconds % 3600) % 60);
-  const ms = String(seconds).split(".")[1] ?? 0;
-  return `${h.toString().padStart(2, "0")}:${m.toString().padStart(2, "0")}:${s
-    .toString()
-    .padStart(2, "0")}.${ms.toString().substring(0, 3).padStart(3, "0")}`;
+  const h = Math.floor(seconds / 3600).toString();
+  const m = Math.floor((seconds % 3600) / 60).toString();
+  const s = Math.floor((seconds % 3600) % 60).toString();
+  const ms = String(seconds).split(".")[1]?.substring(0, 3) ?? "0";
+  return `${h.padStart(2, "0")}:${m.padStart(2, "0")}:${s.padStart(2, "0")}.${ms.padStart(3, "0")}`;
 }
