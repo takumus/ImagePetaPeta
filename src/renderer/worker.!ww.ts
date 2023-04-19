3ec9d3ba5ba8f0c53658b6d6d8864b320e7dafc6
takumus <me@takumus.io>
2023-04-20 05:23:37 +0800

    いらないコードだった

--- src/renderer/worker.!ww.ts
+++
@@ -1,7 +0,0 @@
-import { initWebWorker } from "@/renderer/initWebWorker";
-
-export default initWebWorker<{ msg: string }, { msg: string }>(self, (worker) => {
-  worker.addEventListener("message", (e) => {
-    worker.postMessage({ msg: e.data.msg.split("").reverse().join("") });
-  });
-});
