f69ea4ebecf8398e0f71fff7174b23607f57d5d2
takumus <me@takumus.io>
2023-05-06 02:03:32 +0900

    windowsの最ニングテスト。

--- scripts/buildElectron.ts
+++ scripts/buildElectron.ts
@@ -1,53 +1,78 @@
 import { electronConfiguration } from "../electron.config";
 import { Arch, Platform, build } from "electron-builder";
+import { writeFileSync } from "fs";
 import { exit } from "process";
 import { createRelease } from "scripts/createRelease";
+import yargs from "yargs/yargs";
 
-const target = (() => {
-  const [os, archString] = process.argv
-    .find((arg) => arg.startsWith("--electron-target"))
-    ?.split("=")[1]
-    .split(":")
-    .map((arg) => arg.trim().toLocaleLowerCase()) ?? [undefined, undefined];
-  if (os === undefined || archString === undefined) {
-    console.log("taregt: default");
-    return undefined;
+(async () => {
+  const args = await yargs(process.argv).argv;
+  console.log(args);
+  const [os, archString] = (args["electron-target"] as string | undefined)?.split(":") ?? [];
+  const target = (() => {
+    if (os === undefined || archString === undefined) {
+      console.log("taregt: default");
+      return undefined;
+    }
+    const arch = {
+      ia32: Arch.ia32,
+      x64: Arch.x64,
+      armv7l: Arch.armv7l,
+      arm64: Arch.arm64,
+      universal: Arch.universal,
+    }[archString];
+    if (arch === undefined) {
+      console.log("target: default");
+      return undefined;
+    }
+    console.log(`target: ${os}, ${archString}`);
+    if (os === "mac") {
+      return Platform.MAC.createTarget(undefined, arch);
+    }
+    if (os === "win") {
+      return Platform.WINDOWS.createTarget(undefined, arch);
+    }
+  })();
+  const win: typeof electronConfiguration.win = (() => {
+    if (
+      os !== "win" ||
+      process.env.WINDOWS_PFX_PASSWORD == undefined ||
+      process.env.WINDOWS_PFX == undefined
+    ) {
+      return undefined;
+    }
+    writeFileSync("./windowsPFX.pfx", process.env.WINDOWS_PFX, { encoding: "base64" });
+    return {
+      cscKeyPassword: process.env.WINDOWS_PFX_PASSWORD,
+      cscLink: "./windowsPFX.pfx",
+    };
+  })();
+  if (win !== undefined) {
+    console.log("csc:", win);
   }
-  const arch = {
-    ia32: Arch.ia32,
-    x64: Arch.x64,
-    armv7l: Arch.armv7l,
-    arm64: Arch.arm64,
-    universal: Arch.universal,
-  }[archString];
-  if (arch === undefined) {
-    console.log("target: default");
-    return undefined;
-  }
-  console.log(`target: ${os}, ${archString}`);
-  if (os === "mac") {
-    return Platform.MAC.createTarget(undefined, arch);
-  }
-  if (os === "win") {
-    return Platform.WINDOWS.createTarget(undefined, arch);
-  }
-})();
-build({
-  config: electronConfiguration,
-  publish: "never",
-  targets: target,
-})
-  .then(() => {
-    createRelease()
-      .then(() => {
-        exit(0);
-      })
-      .catch((e) => {
-        console.error(e);
-        exit(-1);
-      });
+  build({
+    config: {
+      ...electronConfiguration,
+      win: {
+        ...electronConfiguration.win,
+        ...win,
+      },
+    },
+    publish: "never",
+    targets: target,
   })
-  .catch((e) => {
-    console.error(e);
-    exit(-1);
-  });
+    .then(() => {
+      createRelease()
+        .then(() => {
+          exit(0);
+        })
+        .catch((e) => {
+          console.error(e);
+          exit(-1);
+        });
+    })
+    .catch((e) => {
+      console.error(e);
+      exit(-1);
+    });
+})();
