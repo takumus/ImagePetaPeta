e0dc890a2a111ec2e63de2877e0f2b22db8b48ea
takumus <me@takumus.io>
2023-05-15 22:48:33 +0800

    わざと失敗

--- test/unit/fileSHA256.test.ts
+++ test/unit/fileSHA256.test.ts
@@ -9,7 +9,7 @@ describe("ileSHA256", () => {
       "da9c463318e720334478f9564c900e6d0432f599ee48d5279a26ac5b21ef81ea",
     );
     expect(await fileSHA256(resolve("./test/sampleDatas/bird.png"))).toBe(
-      "ab9c465faf5c4a6b1f88983938e8e7fcb3c34e91f7293e8a92aece4571630a55",
+      "ab9c465faf5c4a6b1f88983938e8e7fcb3c34e91f7293e8a92aece4571630a55a",
     );
   });
 });
