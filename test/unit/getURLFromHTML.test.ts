65daecb2b0f479b534693906217fa56332e54294
takumus <me@takumus.io>
2023-05-20 22:16:07 +0900

    テスト。

---
+++ test/unit/getURLFromHTML.test.ts
@@ -0,0 +1,42 @@
+import { resolve } from "path";
+import { describe, expect, test } from "vitest";
+
+import { getURLFromHTML } from "@/renderer/utils/getURLFromHTML";
+
+describe("getURLFromHTML", () => {
+  test("src", async () => {
+    const result = getURLFromHTML(`
+    <img src="./test.png">
+    `);
+    expect(result).toMatchObject(["./test.png"]);
+  });
+  test("child", async () => {
+    const result = getURLFromHTML(`
+    <div>
+      <img src="./test.png">
+    </div>
+    `);
+    expect(result).toMatchObject(["./test.png"]);
+  });
+  test("srcset", async () => {
+    const result = getURLFromHTML(`
+    <img srcset="./test-100.png 100w,
+      ./test-200.png 200w,
+      ./test-400.png 400w
+      ./test-300.png 300w">
+    `);
+    console.log(result);
+    expect(result).toMatchObject(["./test-400.png"]);
+  });
+  test("both", async () => {
+    const result = getURLFromHTML(`
+    <img srcset="./test-100.png 100w,
+      ./test-200.png 200w,
+      ./test-400.png 400w
+      ./test-300.png 300w"
+      src="./test.png">
+    `);
+    console.log(result);
+    expect(result).toMatchObject(["./test-400.png", "./test.png"]);
+  });
+});
