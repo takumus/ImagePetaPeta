fe0f860f39a57fc59b70a4b6823c74f16aa9f2c8
takumus <me@takumus.io>
2023-06-08 05:59:07 +0900

    変えた。

--- test/scenario/petaFilesController.test.ts
+++ test/scenario/petaFilesController.test.ts
@@ -24,12 +24,12 @@ describe("petaFilesController", () => {
   });
   beforeEach(async (h) => {
     try {
-      rmdirSync(resolve(ROOT, h.meta.name), { recursive: true });
+      rmdirSync(resolve(ROOT, h.task.name), { recursive: true });
     } catch {
       //
     }
-    mkdirSync(resolve(ROOT, h.meta.name), { recursive: true });
-    await initDummyElectron(resolve(ROOT, h.meta.name));
+    mkdirSync(resolve(ROOT, h.task.name), { recursive: true });
+    await initDummyElectron(resolve(ROOT, h.task.name));
   });
   test("importFilesFromFileInfos(file)", async () => {
     const pfc = usePetaFilesController();
--- test/scenario/webhook.test.ts
+++ test/scenario/webhook.test.ts
@@ -20,12 +20,12 @@ describe("webhook", () => {
   });
   beforeEach(async (h) => {
     try {
-      rmdirSync(resolve(ROOT, h.meta.name), { recursive: true });
+      rmdirSync(resolve(ROOT, h.task.name), { recursive: true });
     } catch {
       //
     }
-    mkdirSync(resolve(ROOT, h.meta.name), { recursive: true });
-    await initDummyElectron(resolve(ROOT, h.meta.name));
+    mkdirSync(resolve(ROOT, h.task.name), { recursive: true });
+    await initDummyElectron(resolve(ROOT, h.task.name));
   });
   async function post<U extends keyof IpcFunctions>(
     apiKey: string,
