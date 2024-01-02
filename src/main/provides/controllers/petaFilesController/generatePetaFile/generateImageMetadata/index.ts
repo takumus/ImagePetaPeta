4da2cd299e2e35d699de4a33e1bbce233c160340
takumus <me@takumus.io>
2024-01-02 22:42:17 +0900

    wtが死んだ。いったんこれで。

--- src/main/provides/controllers/petaFilesController/generatePetaFile/generateImageMetadata/index.ts
+++ src/main/provides/controllers/petaFilesController/generatePetaFile/generateImageMetadata/index.ts
@@ -1,8 +1,24 @@
+import { Worker } from "node:worker_threads";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
 import { WorkerThreadsInputType, WorkerThreadsOutputType } from "@/main/libs/initWorkerThreads";
 import { createWorkerThreadsGroup } from "@/main/libs/workerThreadsGroup";
 import WorkerThreads from "@/main/provides/controllers/petaFilesController/generatePetaFile/generateImageMetadata/generateImageMetadata.!wt";
 
-const wtGroup = createWorkerThreadsGroup(WorkerThreads);
+class W extends Worker {
+  constructor() {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    super(
+      resolve(
+        process.env.TEST === "true" ? `./_test/_wt` : __dirname,
+        "generateImageMetadata.!wt.js",
+      ),
+    );
+  }
+}
+const wtGroup = createWorkerThreadsGroup(W as any as typeof WorkerThreads);
 export async function generateImageMetadataByWorker(
   params: WorkerThreadsInputType<typeof WorkerThreads>,
 ) {
--- vitePlugins/workerThreads.ts
+++ vitePlugins/workerThreads.ts
@@ -4,37 +4,37 @@ export default (pluginOptions?: {}): Plugin => {
   return {
     name: "worker-threads",
     transform(code, id, options) {
-      if (/import.*?from.*?.*?\.!wt["']/g.exec(code)) {
-        const regexp = /import(.*?)from.*?(.*?\.!wt)["']/g;
-        let result: RegExpExecArray | null;
-        const workers: { name: string; file: string }[] = [];
-        while ((result = regexp.exec(code))) {
-          const name = result[1].trim();
-          const file = result[2].trim().split("/").pop();
-          if (file === undefined) {
-            throw new Error("error");
-          }
-          workers.push({
-            name,
-            file,
-          });
-        }
-        const root = process.env.TEST === "true" ? `"./_test/_wt"` : "__dirname";
-        const newCode =
-          `import { Worker as __WT__ } from "worker_threads";\n` +
-          `import { resolve as __RESOLVE__ } from "path";\n` +
-          workers
-            .map(
-              (worker) =>
-                `class ${worker.name} extends __WT__ { constructor() { super(__RESOLVE__(${root}, "${worker.file}.js")); } }\n`,
-            )
-            .join() +
-          code.replace(/(import.*?from.*?.*?\.!wt["'])/g, "//$1");
-        // console.log(newCode);
-        return {
-          code: newCode,
-        };
-      }
+      // if (/import.*?from.*?.*?\.!wt["']/g.exec(code)) {
+      //   const regexp = /import(.*?)from.*?(.*?\.!wt)["']/g;
+      //   let result: RegExpExecArray | null;
+      //   const workers: { name: string; file: string }[] = [];
+      //   while ((result = regexp.exec(code))) {
+      //     const name = result[1].trim();
+      //     const file = result[2].trim().split("/").pop();
+      //     if (file === undefined) {
+      //       throw new Error("error");
+      //     }
+      //     workers.push({
+      //       name,
+      //       file,
+      //     });
+      //   }
+      //   const root = process.env.TEST === "true" ? `"./_test/_wt"` : "__dirname";
+      //   const newCode =
+      //     `import { Worker as __WT__ } from "worker_threads";\n` +
+      //     `import { resolve as __RESOLVE__ } from "path";\n` +
+      //     workers
+      //       .map(
+      //         (worker) =>
+      //           `class ${worker.name} extends __WT__ { constructor() { super(__RESOLVE__(${root}, "${worker.file}.js")); } }\n`,
+      //       )
+      //       .join() +
+      //     code.replace(/(import.*?from.*?.*?\.!wt["'])/g, "//$1");
+      //   // console.log(newCode);
+      //   return {
+      //     code: newCode,
+      //   };
+      // }
     },
   };
 };
