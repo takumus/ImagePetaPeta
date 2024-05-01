31b9cc2499bf69599b07440cd804354c8e7d4f29
takumus <me@takumus.io>
2024-05-01 21:10:29 +0900

    add options

--- src/main/utils/encryptFile.ts
+++ src/main/utils/encryptFile.ts
@@ -3,16 +3,23 @@ import { createReadStream, createWriteStream } from "fs";
 import { PassThrough, pipeline } from "stream";
 
 type Mode = "encrypt" | "decrypt";
+type ReadStreamOptions = Parameters<typeof createReadStream>[1];
 export const secureFile = ((iv: Buffer) => {
   const ALGORITHM = "aes-192-cbc" as const;
   function getKey(key: string) {
     // 192bitのキー
     return createHash("sha256").update(key).digest("base64").substring(0, 24);
   }
-  function asFile(inputFilePath: string, outputFilePath: string, key: string, mode: Mode) {
+  function asFile(
+    inputFilePath: string,
+    outputFilePath: string,
+    key: string,
+    mode: Mode,
+    options?: ReadStreamOptions,
+  ) {
     return new Promise<void>((res, rej) => {
       const output = createWriteStream(outputFilePath);
-      const encoded = asStream(inputFilePath, key, mode);
+      const encoded = asStream(inputFilePath, key, mode, options);
       encoded.pipe(output);
       function error(err: any) {
         rej(err);
@@ -24,13 +31,13 @@ export const secureFile = ((iv: Buffer) => {
       encoded.on("end", res);
     });
   }
-  function asStream(inputFilePath: string, key: string, mode: Mode) {
+  function asStream(inputFilePath: string, key: string, mode: Mode, options?: ReadStreamOptions) {
     const decipher = (mode === "encrypt" ? createCipheriv : createDecipheriv)(
       ALGORITHM,
       getKey(key),
       iv,
     );
-    const input = createReadStream(inputFilePath);
+    const input = createReadStream(inputFilePath, options);
     const transformed = new PassThrough();
     pipeline(input, decipher, transformed, (err) => {
       if (!err) return;
@@ -42,9 +49,14 @@ export const secureFile = ((iv: Buffer) => {
   }
   function createFunctions(mode: Mode) {
     return {
-      asFile: (inputFilePath: string, outputFilePath: string, key: string) =>
-        asFile(inputFilePath, outputFilePath, key, mode),
-      asStream: (inputFilePath: string, key: string) => asStream(inputFilePath, key, mode),
+      asFile: (
+        inputFilePath: string,
+        outputFilePath: string,
+        key: string,
+        readStreamOptions?: ReadStreamOptions,
+      ) => asFile(inputFilePath, outputFilePath, key, mode, readStreamOptions),
+      asStream: (inputFilePath: string, key: string, readStreamOptions?: ReadStreamOptions) =>
+        asStream(inputFilePath, key, mode, readStreamOptions),
     };
   }
   return {
