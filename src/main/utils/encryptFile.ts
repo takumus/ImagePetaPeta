ea381047a4331da88f944fdc903473e2fdd0a9aa
takumus <me@takumus.io>
2024-05-01 15:44:38 +0900

    streamとしても得られる。

--- src/main/utils/encryptFile.ts
+++ src/main/utils/encryptFile.ts
@@ -1,23 +1,29 @@
 import { createCipheriv, createDecipheriv, createHash } from "crypto";
 import { createReadStream, createWriteStream } from "fs";
+import { PassThrough, pipeline, Readable } from "stream";
+import { promisify } from "util";
 
+const algorithm = "aes-192-cbc" as const;
+const iv = Buffer.alloc(16, 0);
 function getKey(key: string) {
   return createHash("sha256").update(key).digest("base64").substring(0, 24);
 }
 export const encryptFile = (inputFilePath: string, outputFilePath: string, key: string) => {
-  return new Promise<void>((res) => {
-    const inputReadStream = createReadStream(inputFilePath);
-    const outputWriteStream = createWriteStream(outputFilePath);
-    const cipher = createCipheriv("aes-192-cbc", getKey(key), Buffer.alloc(16, 0));
-    let encrypted;
-    inputReadStream.on("data", (data) => {
-      encrypted = cipher.update(data);
-      outputWriteStream.write(encrypted);
+  return new Promise<void>((res, rej) => {
+    const input = createReadStream(inputFilePath);
+    const output = createWriteStream(outputFilePath);
+    const cipher = createCipheriv(algorithm, getKey(key), iv);
+    input.on("data", (data) => {
+      output.write(cipher.update(data));
     });
-    inputReadStream.on("end", () => {
-      outputWriteStream.write(cipher.final());
-      outputWriteStream.end();
-      outputWriteStream.close(() => {
+    input.on("end", () => {
+      try {
+        output.write(cipher.final());
+      } catch (err) {
+        rej(err);
+      }
+      output.end();
+      output.close(() => {
         res();
       });
     });
@@ -25,25 +31,39 @@ export const encryptFile = (inputFilePath: string, outputFilePath: string, key: 
 };
 export const decryptFile = (inputFilePath: string, outputFilePath: string, key: string) => {
   return new Promise<void>((res, rej) => {
-    const outputWriteSteam = createWriteStream(outputFilePath);
-    const inputReadStream = createReadStream(inputFilePath);
-
-    const decipher = createDecipheriv("aes-192-cbc", getKey(key), Buffer.alloc(16, 0));
-    let decrypted;
-    inputReadStream.on("data", (data) => {
-      decrypted = decipher.update(data as Buffer);
-      outputWriteSteam.write(decrypted);
+    const output = createWriteStream(outputFilePath);
+    const input = createReadStream(inputFilePath);
+    const cipher = createDecipheriv(algorithm, getKey(key), iv);
+    input.on("data", (data) => {
+      output.write(cipher.update(data as Buffer));
     });
-    inputReadStream.on("end", () => {
+    input.on("end", () => {
       try {
-        outputWriteSteam.write(decipher.final());
+        output.write(cipher.final());
       } catch (err) {
         rej(err);
       }
-      outputWriteSteam.end();
-      outputWriteSteam.close(() => {
+      output.end();
+      output.close(() => {
         res();
       });
     });
   });
+};
+
+export const decryptFileStream = (inputFilePath: string, key: string) => {
+  const decipher = createDecipheriv(algorithm, getKey(key), iv);
+  const input = createReadStream(inputFilePath);
+  // pipelineを用いて、inputとdecipherを連結し、decryptStreamにデータをpushする
+  const passThrough = new PassThrough();
+  pipeline(input, decipher, passThrough, (err) => {
+    if (err) {
+      // エラーハンドリング：passThroughストリームにエラーを通知
+      passThrough.emit("error", err);
+      return;
+    }
+    // 終了時の処理が必要であればここに記述
+  });
+
+  return passThrough;
 };
