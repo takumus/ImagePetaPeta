f65ab0566a368ef7df00a1ffb14e2cb1b47bc3eb
takumus <me@takumus.io>
2024-05-01 16:09:04 +0900

    シンプルに。

--- src/main/utils/encryptFile.ts
+++ src/main/utils/encryptFile.ts
@@ -10,60 +10,65 @@ function getKey(key: string) {
 }
 export const encryptFile = (inputFilePath: string, outputFilePath: string, key: string) => {
   return new Promise<void>((res, rej) => {
-    const input = createReadStream(inputFilePath);
     const output = createWriteStream(outputFilePath);
-    const cipher = createCipheriv(algorithm, getKey(key), iv);
-    input.on("data", (data) => {
-      output.write(cipher.update(data));
+    const input = createEncryptFileStream(inputFilePath, key);
+    input.pipe(output);
+    input.on("error", (err) => {
+      rej();
+      output.destroy(err);
+      output.end();
+    });
+    output.on("error", (err) => {
+      rej();
     });
     input.on("end", () => {
-      try {
-        output.write(cipher.final());
-      } catch (err) {
-        rej(err);
-      }
-      output.end();
-      output.close(() => {
-        res();
-      });
+      res();
     });
   });
 };
 export const decryptFile = (inputFilePath: string, outputFilePath: string, key: string) => {
   return new Promise<void>((res, rej) => {
     const output = createWriteStream(outputFilePath);
-    const input = createReadStream(inputFilePath);
-    const cipher = createDecipheriv(algorithm, getKey(key), iv);
-    input.on("data", (data) => {
-      output.write(cipher.update(data as Buffer));
+    const input = createDecryptFileStream(inputFilePath, key);
+    input.pipe(output);
+    input.on("error", (err) => {
+      rej();
+      output.destroy(err);
+      output.end();
+    });
+    output.on("error", (err) => {
+      rej();
     });
     input.on("end", () => {
-      try {
-        output.write(cipher.final());
-      } catch (err) {
-        rej(err);
-      }
-      output.end();
-      output.close(() => {
-        res();
-      });
+      res();
     });
   });
 };
-
-export const decryptFileStream = (inputFilePath: string, key: string) => {
+export const createDecryptFileStream = (inputFilePath: string, key: string) => {
   const decipher = createDecipheriv(algorithm, getKey(key), iv);
   const input = createReadStream(inputFilePath);
-  // pipelineを用いて、inputとdecipherを連結し、decryptStreamにデータをpushする
   const passThrough = new PassThrough();
   pipeline(input, decipher, passThrough, (err) => {
     if (err) {
-      // エラーハンドリング：passThroughストリームにエラーを通知
       passThrough.emit("error", err);
-      return;
     }
-    // 終了時の処理が必要であればここに記述
   });
-
+  decipher.on("error", (err) => {
+    passThrough.emit("error", err);
+  });
+  return passThrough;
+};
+export const createEncryptFileStream = (inputFilePath: string, key: string) => {
+  const decipher = createCipheriv(algorithm, getKey(key), iv);
+  const input = createReadStream(inputFilePath);
+  const passThrough = new PassThrough();
+  pipeline(input, decipher, passThrough, (err) => {
+    if (err) {
+      passThrough.emit("error", err);
+    }
+  });
+  decipher.on("error", (err) => {
+    passThrough.emit("error", err);
+  });
   return passThrough;
 };
