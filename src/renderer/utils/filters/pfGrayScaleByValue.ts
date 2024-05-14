f7f253a8850e0b7e29f060e24ee3316bf155525b
takumus <me@takumus.io>
2024-05-15 03:51:34 +0900

    フィルター一回無効

--- src/renderer/utils/filters/pfGrayScaleByValue.ts
+++ src/renderer/utils/filters/pfGrayScaleByValue.ts
@@ -1,27 +1,27 @@
-import * as PIXI from "pixi.js";
+// import * as PIXI from "pixi.js";
 
-export class PFGrayScaleByValue extends PIXI.Filter {
-  constructor() {
-    super(
-      undefined,
-      `
-        precision mediump float;
-        varying vec2 vTextureCoord;
-        uniform sampler2D uSampler;
-        uniform float ratio;
-        void main(void) {
-          vec4 color = texture2D(uSampler, vTextureCoord);
-          float y = (
-            max(max(color.r, color.g), color.b)
-            + min(min(color.r, color.g), color.b)
-          ) / 2.0;
-          color.r = mix(color.r, y, ratio);
-          color.g = mix(color.g, y, ratio);
-          color.b = mix(color.b, y, ratio);
-          gl_FragColor = color;
-        }
-      `,
-      { ratio: 1 },
-    );
-  }
-}
+// export class PFGrayScaleByValue extends PIXI.Filter {
+//   constructor() {
+//     super(
+//       undefined,
+//       `
+//         precision mediump float;
+//         varying vec2 vTextureCoord;
+//         uniform sampler2D uSampler;
+//         uniform float ratio;
+//         void main(void) {
+//           vec4 color = texture2D(uSampler, vTextureCoord);
+//           float y = (
+//             max(max(color.r, color.g), color.b)
+//             + min(min(color.r, color.g), color.b)
+//           ) / 2.0;
+//           color.r = mix(color.r, y, ratio);
+//           color.g = mix(color.g, y, ratio);
+//           color.b = mix(color.b, y, ratio);
+//           gl_FragColor = color;
+//         }
+//       `,
+//       { ratio: 1 },
+//     );
+//   }
+// }
--- src/renderer/utils/filters/pfTest.ts
+++ src/renderer/utils/filters/pfTest.ts
@@ -1,35 +1,35 @@
-import * as PIXI from "pixi.js";
+// import * as PIXI from "pixi.js";
 
-export class PFTest extends PIXI.Filter {
-  constructor() {
-    super(
-      undefined,
-      `
-        #define PI 3.1415926535897932384626433832795
-        precision mediump float;
-        varying vec2 vTextureCoord;
-        uniform sampler2D uSampler;
-        uniform float ratio;
-        vec3 hsv2rgb(vec3 c) {
-          vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-          vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-          return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-        }
-        void main(void) {
-          // vec4 color = texture2D(uSampler, vTextureCoord);
-          vec2 c = vec2(0.5, 0.5);
-          float radius = 0.4;
-          vec2 d = vTextureCoord - c;
-          float distance = sqrt(d.x * d.x + d.y * d.y);
-          if (distance < radius) {
-            float radian = atan(d.y, d.x) / PI / 2.0;
-            gl_FragColor = vec4(hsv2rgb(vec3(radian, distance / radius, 1.0)), 1.0);
-          } else {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-          }
-        }
-      `,
-      { ratio: 1 },
-    );
-  }
-}
+// export class PFTest extends PIXI.Filter {
+//   constructor() {
+//     super(
+//       undefined,
+//       `
+//         #define PI 3.1415926535897932384626433832795
+//         precision mediump float;
+//         varying vec2 vTextureCoord;
+//         uniform sampler2D uSampler;
+//         uniform float ratio;
+//         vec3 hsv2rgb(vec3 c) {
+//           vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+//           vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+//           return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+//         }
+//         void main(void) {
+//           // vec4 color = texture2D(uSampler, vTextureCoord);
+//           vec2 c = vec2(0.5, 0.5);
+//           float radius = 0.4;
+//           vec2 d = vTextureCoord - c;
+//           float distance = sqrt(d.x * d.x + d.y * d.y);
+//           if (distance < radius) {
+//             float radian = atan(d.y, d.x) / PI / 2.0;
+//             gl_FragColor = vec4(hsv2rgb(vec3(radian, distance / radius, 1.0)), 1.0);
+//           } else {
+//             gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+//           }
+//         }
+//       `,
+//       { ratio: 1 },
+//     );
+//   }
+// }
