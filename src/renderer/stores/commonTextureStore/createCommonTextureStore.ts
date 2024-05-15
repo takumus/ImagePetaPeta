710eb587d04a1e0daee358ebf710d17ce0ecfcdb
takumus <me@takumus.io>
2024-05-15 23:30:40 +0900

    共通テクスチャに追加。

--- src/renderer/stores/commonTextureStore/createCommonTextureStore.ts
+++ src/renderer/stores/commonTextureStore/createCommonTextureStore.ts
@@ -4,12 +4,14 @@ import { InjectionKey } from "vue";
 import LOADINGImage from "@/_public/images/textures/loading.png";
 import NOIMAGEImage from "@/_public/images/textures/noImage.png";
 import NSFWImage from "@/_public/images/textures/nsfw.png";
+import hsvCircleImage from "@/_public/images/utils/hsvCircle.png";
 
 export async function createCommonTextureStore() {
   return {
     NSFW: await Assets.load(NSFWImage),
     NO: await Assets.load(NOIMAGEImage),
     LOADING: await Assets.load(LOADINGImage),
+    HSV_CIRCLE: await Assets.load(hsvCircleImage),
   } as const;
 }
 export type CommonTextureStore = Awaited<ReturnType<typeof createCommonTextureStore>>;
