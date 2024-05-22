84a018b5ecbe032761567c4e5066940605f0ea33
takumus <me@takumus.io>
2024-05-23 08:02:01 +0900

    Update tfImageClassification.ts

--- src/main/libs/tf/tfImageClassification.ts
+++ src/main/libs/tf/tfImageClassification.ts
@@ -8,14 +8,6 @@ import { extraFiles } from "@/_defines/extraFiles";
 import { mobilenetURLToFilename } from "@/main/utils/mobilenetURLToFileName";
 import { resolveExtraFilesPath } from "@/main/utils/resolveExtraFilesPath";
 
-function minURL(url: string) {
-  const pad1 = 20;
-  const pad2 = 30;
-  if (url.length < pad1 + pad2) {
-    return url;
-  }
-  return url.slice(0, pad1) + "..." + url.slice(-pad2);
-}
 export class TFImageClassification {
   model: tf.GraphModel<string | tf.io.IOHandler> | undefined;
   constructor() {}
@@ -37,26 +29,19 @@ export class TFImageClassification {
                 filename as keyof (typeof extraFiles)["mobilenet.universal"]
               ],
             );
-            console.log("TF:", "override fetch!", minURL(url), "->", minURL(path));
+            console.log("TF:", "override fetch!", this.minURL(url), "->", this.minURL(path));
             await stat(path);
             return new Response(await readFile(path));
           } else {
-            console.log("TF:", "through fetch!", minURL(url));
+            console.log("TF:", "through fetch!", this.minURL(url));
             return fetch(url);
           }
         },
       },
     );
   }
-  preprocessImage(data: Buffer, info: sharp.OutputInfo) {
-    return tf
-      .tensor(data, [info.height, info.width, info.channels])
-      .toFloat()
-      .div(tf.scalar(255))
-      .expandDims();
-  }
   async imageToTensor(source: string | Buffer) {
-    const { data, info } = await createImageForTensor(source);
+    const { data, info } = await this.createImage(source);
     return tf.tidy(() => {
       if (this.model === undefined) {
         throw "mobilenet is not initialized";
@@ -73,25 +58,40 @@ export class TFImageClassification {
       return dotProduct.div(normA.mul(normB)).dataSync()[0];
     });
   }
-}
-async function createImageForTensor(source: string | Buffer) {
-  const noise = Buffer.alloc(224 * 224 * 3);
-  for (let i = 0; i < noise.length; i++) {
-    noise[i] = Math.round(0xff * Math.random());
+  private preprocessImage(data: Buffer, info: sharp.OutputInfo) {
+    return tf
+      .tensor(data, [info.height, info.width, info.channels])
+      .toFloat()
+      .div(tf.scalar(255))
+      .expandDims();
   }
-  const img = await sharp(source)
-    .resize(224, 224, {
-      fit: "inside",
-    })
-    .removeAlpha()
-    .toBuffer();
-  return await sharp(noise, { raw: { width: 224, height: 224, channels: 3 } })
-    .composite([
-      {
-        input: img,
-      },
-    ])
-    .raw()
-    .removeAlpha()
-    .toBuffer({ resolveWithObject: true });
+  private async createImage(source: string | Buffer) {
+    const noise = Buffer.alloc(224 * 224 * 3);
+    for (let i = 0; i < noise.length; i++) {
+      noise[i] = Math.round(0xff * Math.random());
+    }
+    const img = await sharp(source)
+      .resize(224, 224, {
+        fit: "inside",
+      })
+      .removeAlpha()
+      .toBuffer();
+    return await sharp(noise, { raw: { width: 224, height: 224, channels: 3 } })
+      .composite([
+        {
+          input: img,
+        },
+      ])
+      .raw()
+      .removeAlpha()
+      .toBuffer({ resolveWithObject: true });
+  }
+  private minURL(url: string) {
+    const pad1 = 20;
+    const pad2 = 30;
+    if (url.length < pad1 + pad2) {
+      return url;
+    }
+    return url.slice(0, pad1) + "..." + url.slice(-pad2);
+  }
 }
