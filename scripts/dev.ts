0c131558e641e84c4f793f09b5a8ebcf22e24f1c
takumus <me@takumus.io>
2024-05-22 22:14:47 +0900

    Update dev.ts

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -6,27 +6,7 @@ keypress(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.on("keypress", async function (_ch, key) {
   if (key?.ctrl && key?.name === "c") {
-    await Promise.all(
-      childProcesses.map(
-        (cp) =>
-          new Promise<void>((res, rej) => {
-            if (cp.pid === undefined) {
-              rej("pid is undefined");
-              return;
-            }
-            kill(cp.pid, (e) => {
-              console.log("tree-kill", cp.pid);
-              if (e) {
-                console.error(e);
-                rej(e);
-                return;
-              }
-              res();
-            });
-          }),
-      ),
-    );
-    process.exit(0);
+    killAll();
   }
 });
 const childProcesses = [
@@ -35,4 +15,39 @@ const childProcesses = [
 ];
 childProcesses.forEach((cp) => {
   cp.stdout?.pipe(process.stdout);
+  cp.on("close", () => {
+    killAll();
+  });
 });
+const killAll = (() => {
+  let killed = false;
+  return async () => {
+    if (killed) return;
+    killed = true;
+    try {
+      await Promise.all(
+        childProcesses.map(
+          (cp) =>
+            new Promise<void>((res, rej) => {
+              if (cp.pid === undefined) {
+                res();
+                return;
+              }
+              kill(cp.pid, (e) => {
+                console.log("tree-kill", cp.pid);
+                if (e) {
+                  rej(e);
+                  return;
+                }
+                res();
+              });
+            }),
+        ),
+      );
+    } catch {
+      //
+    }
+    console.log("bye");
+    process.exit(0);
+  };
+})();
