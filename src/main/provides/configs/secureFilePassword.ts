2e9970272d7af43ebb86ada232767676d6206ae7
takumus <me@takumus.io>
2024-05-29 03:33:10 +0900

    Update secureFilePassword.ts

--- src/main/provides/configs/secureFilePassword.ts
+++ src/main/provides/configs/secureFilePassword.ts
@@ -1,12 +1,10 @@
 import { safeStorage } from "electron";
-import { v4 } from "uuid";
 
 import Config from "@/main/libs/config";
 import { passwordToKey } from "@/main/utils/secureFile";
 
 export class ConfigSecureFilePassword extends Config<string> {
   private key: string | undefined;
-  private tempFileKey = passwordToKey(v4());
   setValue(value: string) {
     const key = passwordToKey(value);
     this.data = safeStorage.encryptString(key).toString("base64");
