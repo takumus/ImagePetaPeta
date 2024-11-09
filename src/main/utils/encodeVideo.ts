f6abf1b97175981c0bcff992fd980fbba8a9bcdf
takumus <me@takumus.io>
2024-11-10 03:11:34 +0900

    パーセントOK

--- src/main/utils/encodeVideo.ts
+++ src/main/utils/encodeVideo.ts
@@ -27,17 +27,49 @@ export async function encodeVideo(petaFiles: PetaFile[], args: string[]) {
     async (petaFile, index) => {
       const inputFile = getPetaFilePath.fromPetaFile(petaFile).original;
       const outputFile = `${Path.resolve(paths.DIR_TEMP, petaFile.id)}.mp4`;
-      const parameters = ["-i", inputFile, "-b:v", "5000k", "-c:a", "copy", outputFile];
+      const parameters = [
+        "-i",
+        inputFile,
+        "-b:v",
+        "5000k",
+        "-c:a",
+        "copy",
+        outputFile,
+        "-progress",
+        "-",
+        "-nostats",
+      ];
       let percent = 0;
+      let duration = 0;
+      await runExternalApplication(
+        "ffprobe.exe",
+        [
+          "-v",
+          "error",
+          "-show_entries",
+          "format=duration",
+          "-of",
+          "default=noprint_wrappers=1:nokey=1",
+          inputFile,
+        ],
+        "utf8",
+        (l) => {
+          const d = Number(l.trim());
+          if (!isNaN(d)) {
+            duration = d;
+          }
+        },
+      ).promise;
       const childProcess = runExternalApplication("ffmpeg.exe", parameters, "utf8", (l) => {
         l = l.trim();
-        percent = /^\d+\.\d+%$/.test(l) ? Number(l.replace(/%/, "")) : percent;
+        const d = Number(l.split("out_time_ms=")?.[1]?.split("\n")?.[0]);
+        percent = !isNaN(d) ? d : percent;
         log.debug(l);
         task.emitStatus({
           i18nKey: "tasks.encoding",
           progress: {
             all: petaFiles.length,
-            current: index + percent / 100,
+            current: index + percent / (duration * 1000000),
           },
           log: [l],
           status: "progress",
