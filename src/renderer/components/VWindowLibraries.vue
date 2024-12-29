e7c845aca760a12d6b958fa2e2de756bdfbeec8b
takumus <me@takumus.io>
2024-12-29 20:59:06 +0900

    おｋ

--- src/renderer/components/VWindowLibraries.vue
+++ src/renderer/components/VWindowLibraries.vue
@@ -4,8 +4,10 @@
       <VTitleBar :title="t('titles.libraries')"> </VTitleBar>
     </e-top>
     <e-content>
-      <input type="text" v-model="file" />
-      <button @click="openLib">Open</button>
+      <e-lib v-for="(_, key) of libraries" :key="key">
+        <input type="text" v-model="libraries[key]" />
+        <button @click="openLib(libraries[key])">Open</button>
+      </e-lib>
     </e-content>
     <VContextMenu :z-index="4" />
     <VTooltip :z-index="3" />
@@ -13,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 import VTitleBar from "@/renderer/components/commons/titleBar/VTitleBar.vue";
@@ -21,18 +23,23 @@ import VContextMenu from "@/renderer/components/commons/utils/contextMenu/VConte
 import VTooltip from "@/renderer/components/commons/utils/tooltip/VTooltip.vue";
 import VSettings from "@/renderer/components/settings/VSettings.vue";
 
+import { Libraries } from "@/commons/datas/libraries";
+
 import { IPC } from "@/renderer/libs/ipc";
 import { useAppInfoStore } from "@/renderer/stores/appInfoStore/useAppInfoStore";
 import { useKeyboardsStore } from "@/renderer/stores/keyboardsStore/useKeyboardsStore";
+import { useLibrariesStore } from "@/renderer/stores/librariesStore/useLibrariesStore";
 import { useWindowNameStore } from "@/renderer/stores/windowNameStore/useWindowNameStore";
 import { useWindowTitleStore } from "@/renderer/stores/windowTitleStore/useWindowTitleStore";
 
 const { t } = useI18n();
+const librariesStore = useLibrariesStore();
 const keyboards = useKeyboardsStore();
 const windowNameStore = useWindowNameStore();
 const windowTitleStore = useWindowTitleStore();
 const appInfoStore = useAppInfoStore();
 const file = ref("");
+const libraries = computed(() => librariesStore.state.value);
 onMounted(() => {
   keyboards.enabled = true;
   keyboards.keys("Escape").up(() => {
@@ -46,8 +53,8 @@ watch(
   },
   { immediate: true },
 );
-function openLib() {
-  IPC.common.selectLibrary({ path: file.value });
+function openLib(lib: Libraries[string]) {
+  IPC.common.selectLibrary(lib);
 }
 </script>
 
